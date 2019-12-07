074710722cd05276f38af812dd37bebcb1636eab
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-07 14:24:24 +0100

    Added an instrument listener interface.

---
+++ src/main/java/org/javajdj/jinstrument/r1/instrument/InstrumentListener.java
@@ -0,0 +1,11 @@
+package org.javajdj.jinstrument.r1.instrument;
+
+/**
+ *
+ */
+public interface InstrumentListener
+{
+  
+  void newInstrumentSettings (Instrument instrument, InstrumentSettings instrumentSettings);
+  
+}
