90711b726a0bbdcec193a570077e4643b9c497dd
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-08 14:04:53 +0100

    Added support for reporting InstrumentStatus.

--- src/main/java/org/javajdj/jinstrument/r1/instrument/InstrumentListener.java
+++ src/main/java/org/javajdj/jinstrument/r1/instrument/InstrumentListener.java
@@ -6,6 +6,8 @@ package org.javajdj.jinstrument.r1.instrument;
 public interface InstrumentListener
 {
   
+  void newInstrumentStatus (Instrument instrument, InstrumentStatus instrumentStatus);
+  
   void newInstrumentSettings (Instrument instrument, InstrumentSettings instrumentSettings);
   
 }
