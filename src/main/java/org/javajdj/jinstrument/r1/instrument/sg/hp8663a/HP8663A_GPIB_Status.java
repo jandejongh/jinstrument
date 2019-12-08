76016e4f194f89f381eb9a61df25ba0402d37379
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-08 13:55:28 +0100

    Added InstrumentStatus and an implementation for the HP-8663A.

---
+++ src/main/java/org/javajdj/jinstrument/r1/instrument/sg/hp8663a/HP8663A_GPIB_Status.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.r1.instrument.sg.hp8663a;
+
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import org.javajdj.jinstrument.r1.instrument.InstrumentStatus;
+
+/**
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ */
+public class HP8663A_GPIB_Status
+  implements InstrumentStatus
+{
+  
+  public final static int STATUS_LENGTH = 8;
+  
+  private final byte[] bytes;
+
+  private final String string;
+  
+  public HP8663A_GPIB_Status (final byte[] bytes)
+  {
+    if (bytes == null || bytes.length != HP8663A_GPIB_Status.STATUS_LENGTH)
+      throw new IllegalArgumentException ();
+    this.bytes = bytes;
+    this.string = new String (this.bytes, Charset.forName ("US-ASCII"));
+  }
+
+  @Override
+  public String toString ()
+  {
+    return this.string;
+  }
+
+  @Override
+  public int hashCode ()
+  {
+    int hash = 7;
+    hash = 59 * hash + Arrays.hashCode (this.bytes);
+    return hash;
+  }
+
+  @Override
+  public boolean equals (Object obj)
+  {
+    if (this == obj)
+    {
+      return true;
+    }
+    if (obj == null)
+    {
+      return false;
+    }
+    if (getClass () != obj.getClass ())
+    {
+      return false;
+    }
+    final HP8663A_GPIB_Status other = (HP8663A_GPIB_Status) obj;
+    if (!Arrays.equals (this.bytes, other.bytes))
+    {
+      return false;
+    }
+    return true;
+  }
+  
+}
