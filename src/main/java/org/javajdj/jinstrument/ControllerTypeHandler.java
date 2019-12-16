4bf78e0435042dc1a5bd090235b7ac7a6d34e16c
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-16 19:57:42 +0100

    Moved ControllerTypeHandler; added javadoc.

---
+++ src/main/java/org/javajdj/jinstrument/ControllerTypeHandler.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument;
+
+/** Object capable of generating {@link Controller} objects of some specific type from a URL description.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public interface ControllerTypeHandler
+{
+  
+  boolean isValidUrl (final String controllerUrl);
+  
+  Controller openController (final String controllerUrl);
+  
+}
