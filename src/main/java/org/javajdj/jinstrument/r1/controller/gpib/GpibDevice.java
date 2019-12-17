dc8cfcc01ad4af665bbbd4345a8fed482b7d4420
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-17 11:16:08 +0100

    Extended interface; javadoc.

--- src/main/java/org/javajdj/jinstrument/r1/controller/gpib/GpibDevice.java
+++ src/main/java/org/javajdj/jinstrument/r1/controller/gpib/GpibDevice.java
@@ -1,57 +1,85 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package org.javajdj.jinstrument.r1.controller.gpib;
 
+import org.javajdj.jinstrument.instrument.controller.gpib.GpibAddress;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import org.javajdj.jinstrument.r1.Device;
+import java.util.concurrent.TimeoutException;
+import org.javajdj.jinstrument.Device;
 
-/**
+/** A device on a GPIB (General Purpose Interface Bus).
  *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
  */
-public class GpibDevice
-implements Device
+public interface GpibDevice
+  extends Device
 {
-
-  public GpibDevice (final AbstractGpibController controller, final GpibAddress address)
-  {
-    if (controller == null || address == null)
-      throw new IllegalArgumentException ();
-    this.controller = controller;
-    this.address = address;
-  }
-
-  private final AbstractGpibController controller;
   
-  public final AbstractGpibController getController ()
+  GpibAddress getAddress ();
+  
+  enum ReadlineTerminationMode
   {
-    return this.controller;
+    CR,
+    LF,
+    CR_LF,
+    LF_CR;
   }
   
-  private final GpibAddress address;
+  void readAsync ()
+    throws UnsupportedOperationException;
   
-  public final GpibAddress getAddress ()
-  {
-    return this.address;
-  }
+  byte[] readSync (long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
+    
+  void readlnAsync (ReadlineTerminationMode readlineTerminationMode)
+    throws UnsupportedOperationException;
   
-  public final InputStream getInputStream ()
-  {
-    return this.controller.getInputStream (this);
-  }
+  byte[] readlnSync (ReadlineTerminationMode readlineTerminationMode, long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
+    
+  void readNAsync (final int N)
+    throws UnsupportedOperationException;
   
-  public final OutputStream getOutputStream ()
-  {
-    return this.controller.getOutputStream (this);
-  }
+  byte[] readNSync (final int N, long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
   
-  public final void lockDevice () throws IOException, InterruptedException
-  {
-    this.controller.lockGpibAddress (this.address);
-  }
+  void writeAsync (byte[] bytes)
+    throws UnsupportedOperationException;
   
-  public final void unlockDevice ()
-  {
-    this.controller.unlockGpibAddress (this.address);
-  }
+  void writeSync (byte[] bytes, long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
+  
+  void writeAndReadAsync (byte[] bytes)
+    throws UnsupportedOperationException;
+  
+  byte[] writeAndReadSync (byte[] bytes, long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
+  
+  void writeAndReadlnAsync (byte[] bytes, ReadlineTerminationMode readlineTerminationMode)
+    throws UnsupportedOperationException;
+  
+  byte[] writeAndReadlnSync (byte[] bytes, ReadlineTerminationMode readlineTerminationMode, long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
+  
+  void writeAndReadNAsync (byte[] bytes, int N)
+    throws UnsupportedOperationException;
+  
+  byte[] writeAndReadNSync (byte[] bytes, int N, long timeout_ms)
+    throws InterruptedException, IOException, TimeoutException, UnsupportedOperationException;
   
 }
