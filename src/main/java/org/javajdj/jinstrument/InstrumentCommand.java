1482eebdeac0430c5d9a1cfc1ab9455cdb15669d
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-19 22:11:42 +0100

    Added support for:
    - resolution bandwidth coupling
    - video bandwidth coupling
    - sweep time coupling
    - reference level
    - (rf) attenuation
    - (rf) attenuation coupling
    
    Pretty much finished a "base" spectrum-analyzer settings support.

--- src/main/java/org/javajdj/jinstrument/InstrumentCommand.java
+++ src/main/java/org/javajdj/jinstrument/InstrumentCommand.java
@@ -95,10 +95,24 @@ public interface InstrumentCommand
   public final static String IC_RESOLUTION_BANDWIDTH = "commandResolutionBandwidth";
   public final static String ICARG_RESOLUTION_BANDWIDTH_HZ = "resolutionBandwidth_Hz";
 
+  public final static String IC_SET_RESOLUTION_BANDWIDTH_COUPLED = "commandSetResolutionBandwidthCoupled";
+
   public final static String IC_VIDEO_BANDWIDTH = "commandVideoBandwidth";
   public final static String ICARG_VIDEO_BANDWIDTH_HZ = "videoBandwidth_Hz";
 
+  public final static String IC_SET_VIDEO_BANDWIDTH_COUPLED = "commandSetVideoBandwidthCoupled";
+  
   public final static String IC_SWEEP_TIME = "commandSweepTime";
   public final static String ICARG_SWEEP_TIME_S = "sweepTime_s";
 
+  public final static String IC_SET_SWEEP_TIME_COUPLED = "commandSetSweepTimeCoupled";
+  
+  public final static String IC_REFERENCE_LEVEL = "commandReferenceLevel";
+  public final static String ICARG_REFERENCE_LEVEL_DBM = "referenceLevel_dBm";
+
+  public final static String IC_RF_ATTENUATION = "commandRfAttenuation";
+  public final static String ICARG_RF_ATTENUATION_DB = "rfAttenuation_dB";
+
+  public final static String IC_SET_RF_ATTENUATION_COUPLED = "commandSetRfAttenuationCoupled";
+  
 }
