734fcacd6aa195a3f49c98319dcafff653095f32
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-22 16:39:49 +0100

    Refactored ProLogixGpibEthernetControllerTypeHandler -> ProLogixGpibEthernetControllerType. Added getNanme (), toString (), getBusType.

---
+++ src/main/java/org/javajdj/jinstrument/controller/gpib/ProLogixGpibEthernetControllerType.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.controller.gpib;
+
+import org.javajdj.jinstrument.BusType;
+import org.javajdj.jinstrument.Controller;
+import org.javajdj.jinstrument.ControllerType;
+
+/** Implementation of {@link ControllerType} for the Prologix GPIB-ETHERNET controller.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class ProLogixGpibEthernetControllerType
+implements ControllerType
+{
+
+  private ProLogixGpibEthernetControllerType ()
+  {
+  }
+  
+  private final static ProLogixGpibEthernetControllerType INSTANCE = new ProLogixGpibEthernetControllerType ();
+  
+  public final static ProLogixGpibEthernetControllerType getInstance ()
+  {
+    return ProLogixGpibEthernetControllerType.INSTANCE;
+  }
+
+  @Override
+  public final String getName ()
+  {
+    return "prologix-eth";
+  }
+
+  @Override
+  public String toString ()
+  {
+    return getName ();
+  }
+  
+  @Override
+  public final BusType getBusType ()
+  {
+    return BusType_GPIB.getInstance ();
+  }
+  
+  public final static String PROLOGIX_URL_PREFIX = "prologix-eth://";
+  
+  public final static boolean isValidUrlStatic (final String controllerUrl)
+  {
+    if (controllerUrl == null)
+      return false;
+    return controllerUrl.trim ().toLowerCase ().startsWith (PROLOGIX_URL_PREFIX);    
+  }
+  
+  @Override
+  public final boolean isValidUrl (final String controllerUrl)
+  {
+    return ProLogixGpibEthernetControllerType.isValidUrlStatic (controllerUrl);
+  }
+
+  public final static int DEFAULT_TCP_CONNECT_PORT = 1234;
+  
+  @Override
+  public final Controller openController (final String controllerUrl)
+  {
+    if (! isValidUrl (controllerUrl))
+      return null;
+    final String addressAndPortString =
+      controllerUrl.replaceFirst (ProLogixGpibEthernetControllerType.PROLOGIX_URL_PREFIX, "");
+    final String ipAddress;
+    final int tcpConnectPort;
+    if (! addressAndPortString.contains (":"))
+    {
+      ipAddress = addressAndPortString;
+      tcpConnectPort = ProLogixGpibEthernetControllerType.DEFAULT_TCP_CONNECT_PORT;
+    }
+    else
+    {
+      ipAddress = addressAndPortString.replaceFirst (":.*", "");
+      try
+      {
+        tcpConnectPort = Integer.parseInt (addressAndPortString.replaceFirst (".*:", ""));   
+        if (tcpConnectPort < 0 || tcpConnectPort > 65535)
+          return null;
+      }
+      catch (NumberFormatException nfe)
+      {
+        return null;
+      }
+    }
+    return new ProLogixGpibEthernetController (ipAddress, tcpConnectPort);
+  }
+  
+}
