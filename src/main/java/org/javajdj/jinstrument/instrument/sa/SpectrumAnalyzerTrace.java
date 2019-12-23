dd62f04c11fa2d9dfb07a209766534bfc2709cdf
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-23 13:41:10 +0100

    The {@link InstrumentReading} for a {@link SpectrumAnalyzer}.

---
+++ src/main/java/org/javajdj/jinstrument/instrument/sa/SpectrumAnalyzerTrace.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.instrument.sa;
+
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentTrace;
+
+/** The {@link InstrumentReading} for a {@link SpectrumAnalyzer}.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public interface SpectrumAnalyzerTrace
+extends InstrumentTrace
+{
+  
+  @Override
+  SpectrumAnalyzerSettings getInstrumentSettings ();
+  
+  double sampleIndexToFrequency_MHz (double sampleIndex);
+
+  double frequency_MHzToSampleIndex (double frequency_MHz);
+
+}
