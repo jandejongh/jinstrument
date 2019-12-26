9daf10b39f6738916468818ace895e80ab87793b
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-26 09:57:44 +0100

    A Swing (base) component for interacting with a {@link DigitalMultiMeter}.

---
+++ src/main/java/org/javajdj/jinstrument/swing/JDigitalMultiMeterPanel.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing;
+
+import java.awt.Color;
+import org.javajdj.jinstrument.instrument.dmm.DigitalMultiMeter;
+
+/** A Swing (base) component for interacting with a {@link DigitalMultiMeter}.
+ *
+ * @author {@literal Jan de Jongh <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JDigitalMultiMeterPanel
+  extends JInstrumentPanel
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  protected JDigitalMultiMeterPanel (
+    final DigitalMultiMeter digitalMultiMeter,
+    final String title,
+    final int level,
+    final Color panelColor)
+  {
+    super (digitalMultiMeter, title, level, panelColor);
+    if (digitalMultiMeter == null)
+      throw new IllegalArgumentException ();
+  }
+
+  protected JDigitalMultiMeterPanel (final DigitalMultiMeter digitalMultiMeter, final int level)
+  {
+    this (digitalMultiMeter, null, level, null);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DIGITAL MULTIMETER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  public final DigitalMultiMeter getDigitalMultiMeter ()
+  {
+    return (DigitalMultiMeter) getInstrument ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
