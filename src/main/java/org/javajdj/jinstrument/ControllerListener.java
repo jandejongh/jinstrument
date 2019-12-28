561dacec2c36f290e38f7bec36a0d3bb877fb7e2
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-28 08:10:17 +0100

    Added controller logging. Javadoc.

--- src/main/java/org/javajdj/jinstrument/ControllerListener.java
+++ src/main/java/org/javajdj/jinstrument/ControllerListener.java
@@ -16,6 +16,8 @@
  */
 package org.javajdj.jinstrument;
 
+import java.time.Instant;
+
 /** A listener to (command completions of) a {@link Controller}.
  *
  * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
@@ -25,6 +27,103 @@ package org.javajdj.jinstrument;
 public interface ControllerListener
 {
   
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONTROLLER CORE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Notification from a controller that is has finished processing (or gave up on) a specific command.
+   * 
+   * <p>
+   * The result of processing the command, including failure indications, are present in the command argument,
+   * but its precise structure is implementation dependent.
+   * 
+   * @param controller        The controller, non-{@code null}.
+   * @param controllerCommand The command, non-{@code null}.
+   * 
+   */
   void processedCommandNotification (Controller controller, ControllerCommand controllerCommand);
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONTROLLER LOG
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  /** Notification of logging (debug) output from a controller.
+   * 
+   * @param controller The controller, non-{@code null}.
+   * @param logEntry   The log entry, non-{@code null}.
+   * 
+   */
+  default void controllerLogEntry (Controller controller, LogEntry logEntry) { }
+  
+  /** A log entry type.
+   * 
+   */
+  enum LogEntryType
+  {
+    /** Command has been queued at a controller.
+     * 
+     */
+    LOG_QUEUE_COMMAND,
+    /** Command processing has started at a controller.
+     * 
+     */
+    LOG_START_COMMAND,
+    /** Command processing has ended at a controller.
+     * 
+     */
+    LOG_END_COMMAND,
+    /** Free-format logging / debug message from a controller.
+     * 
+     */
+    LOG_MESSAGE,
+    /** Notification of bytes sent from a controller object to the actual controller device.
+     * 
+     */
+    LOG_TX,
+    /** Notification of bytes received by a controller object from the actual controller device.
+     * 
+     */
+    LOG_RX;  
+  }
+  
+  /** A log entry.
+   * 
+   * @see ControllerListener#controllerLogEntry
+   * 
+   */
+  class LogEntry
+  {
+    
+    public LogEntry (
+      final LogEntryType logEntryType,
+      final Instant instant,
+      final ControllerCommand controllerCommand,
+      final String message,
+      final byte[] bytes)
+    {
+      this.logEntryType = logEntryType;
+      this.instant = instant;
+      this.controllerCommand = controllerCommand;
+      this.message = message;
+      this.bytes = bytes;
+    }
+    
+    public final LogEntryType logEntryType;
+    public final Instant instant;
+    public final ControllerCommand controllerCommand;
+    public final String message;
+    public final byte[] bytes;
+    
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
 }
