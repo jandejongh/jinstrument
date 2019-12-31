a40fba50f90a42085f4d6b70966983c212a5300b
Jan de Jongh <jfcmdejongh@gmail.com>
2019-12-31 11:15:59 +0100

    Version -> 0.3.

--- src/main/java/org/javajdj/jinstrument/swing/JInstrument.java
+++ src/main/java/org/javajdj/jinstrument/swing/JInstrument.java
@@ -95,7 +95,7 @@ extends JFrame
   throws HeadlessException
   {
     
-    super (title == null ? "JInstrument V0.3-SNAPSHOT" : title);
+    super (title == null ? "JInstrument V0.3" : title);
     
     setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
     createMenu ();
