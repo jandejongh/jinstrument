d16b44f7e0b48ba038b934d793f63697ac0773e1
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-02 22:56:19 +0100

    A minimum implementation of a (GPIB) {@link Instrument}, doing nothing at all, on a {@link GpibDevice}.

---
+++ src/main/java/org/javajdj/jinstrument/gpib/none/NONE_GPIB_Instrument.java
@@ -0,0 +1,249 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.gpib.none;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.javajdj.jinstrument.AbstractInstrument;
+import org.javajdj.jinstrument.controller.gpib.GpibDevice;
+import org.javajdj.jinstrument.Device;
+import org.javajdj.jinstrument.DeviceType;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentCommand;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentType;
+import org.javajdj.jinstrument.controller.gpib.DeviceType_GPIB;
+
+/** A minimum implementation of a (GPIB) {@link Instrument}, doing nothing at all, on a {@link GpibDevice}.
+ *
+ * <p>
+ * The main purpose of this class is to allow instrument-agnostic access to GPIB controllers
+ * and GPIB devices.
+ * 
+ * <p>
+ * It is also useful as a template for implementation of {@link Instrument} for
+ * GPIB instruments that do not resemble any of the other
+ * abstract base class for GPIB instruments.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class NONE_GPIB_Instrument
+extends AbstractInstrument
+implements Instrument
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (NONE_GPIB_Instrument.class.getName ());
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public NONE_GPIB_Instrument (final GpibDevice gpibDevice)
+  {
+    super ("NONE [GPIB]", gpibDevice, null, null, false, false, false, false);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentType INSTRUMENT_TYPE = new InstrumentType ()
+  {
+    
+    @Override
+    public final String getInstrumentTypeUrl ()
+    {
+      return "NONE [GPIB]";
+    }
+    
+    @Override
+    public final DeviceType getDeviceType ()
+    {
+      return DeviceType_GPIB.getInstance ();
+    }
+
+    @Override
+    public final NONE_GPIB_Instrument openInstrument (final Device device)
+    {
+      if (device == null || ! (device instanceof GpibDevice))
+      {
+        LOG.log (Level.WARNING, "Incompatible Device (not a GpibDevice): {0}!", device);
+        return null;
+      }
+      return new NONE_GPIB_Instrument ((GpibDevice) device);
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // Instrument
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentUrl ()
+  {
+    return NONE_GPIB_Instrument.INSTRUMENT_TYPE.getInstrumentTypeUrl () + "@" + getDevice ().getDeviceUrl ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return getInstrumentUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // Instrument
+  // POWER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public boolean isPoweredOn ()
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  @Override
+  public void setPoweredOn (final boolean poweredOn)
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // INSTRUMENT STATUS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  @Override
+  protected InstrumentStatus getStatusFromInstrumentSync ()
+    throws IOException, InterruptedException, TimeoutException
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  @Override
+  protected void requestStatusFromInstrumentASync ()
+    throws UnsupportedOperationException, IOException
+  {
+    throw new UnsupportedOperationException ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // INSTRUMENT SETTINGS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  protected void requestSettingsFromInstrumentASync () throws UnsupportedOperationException, IOException
+  {
+    throw new UnsupportedOperationException ();
+  }
+  
+  @Override
+  public InstrumentSettings getSettingsFromInstrumentSync ()
+    throws IOException, InterruptedException, TimeoutException
+  {
+    throw new UnsupportedOperationException ();
+  }
+ 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // INSTRUMENT READING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  @Override
+  protected final InstrumentReading getReadingFromInstrumentSync () throws IOException, InterruptedException, TimeoutException
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  @Override
+  protected final void requestReadingFromInstrumentASync () throws IOException
+  {
+    throw new UnsupportedOperationException ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // PROCESS COMMAND
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+  @Override
+  protected void processCommand (final InstrumentCommand instrumentCommand)
+    throws IOException, InterruptedException, TimeoutException
+  {
+    if (instrumentCommand == null)
+      throw new IllegalArgumentException ();
+    if (! instrumentCommand.containsKey (InstrumentCommand.IC_COMMAND_KEY))
+      throw new IllegalArgumentException ();
+    if (instrumentCommand.get (InstrumentCommand.IC_COMMAND_KEY) == null)
+      throw new IllegalArgumentException ();
+    if (! (instrumentCommand.get (InstrumentCommand.IC_COMMAND_KEY) instanceof String))
+      throw new IllegalArgumentException ();
+    final String commandString = (String) instrumentCommand.get (InstrumentCommand.IC_COMMAND_KEY);
+    if (commandString.trim ().isEmpty ())
+      throw new IllegalArgumentException ();
+    try
+    {
+      switch (commandString)
+      {
+        case InstrumentCommand.IC_NOP_KEY:
+          break;
+        default:
+          throw new UnsupportedOperationException ();
+      }
+    }
+    finally
+    {
+      // EMPTY
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+}
