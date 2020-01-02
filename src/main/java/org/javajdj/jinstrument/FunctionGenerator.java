2917e45e8b88cc8751723683620714955851ce03
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-02 16:08:45 +0100

    Refactored: Moved into top-level package.

---
+++ src/main/java/org/javajdj/jinstrument/FunctionGenerator.java
@@ -0,0 +1,46 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument;
+
+import java.io.IOException;
+
+/** Extension of {@link Instrument} for (typically LF) single-channel function generators.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public interface FunctionGenerator
+  extends Instrument
+{
+  
+  enum Waveform
+  {
+    DC,
+    SINE,
+    TRIANGLE,
+    RAMP_UP,
+    RAMP_DOWN,
+    SQUARE,
+    PULSE;
+  }
+
+  void setWaveform (Waveform Waveform);
+  
+  void setFrequency_Hz (double centerFrequency_Hz)
+    throws IOException, InterruptedException;
+
+}
