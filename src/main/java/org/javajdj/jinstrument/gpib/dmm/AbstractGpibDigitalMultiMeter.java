b5a60eeb649799221c4136a7b09140f75213f1d1
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-02 22:48:49 +0100

    Moved class. Now inherits from AbstractGpibInstrument.

---
+++ src/main/java/org/javajdj/jinstrument/gpib/dmm/AbstractGpibDigitalMultiMeter.java
@@ -0,0 +1,112 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.gpib.dmm;
+
+import org.javajdj.jinstrument.DigitalMultiMeter;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+import org.javajdj.jinstrument.DefaultInstrumentCommand;
+import org.javajdj.jinstrument.InstrumentCommand;
+import org.javajdj.jinstrument.gpib.AbstractGpibInstrument;
+import org.javajdj.jinstrument.controller.gpib.GpibDevice;
+import org.javajdj.jservice.Service;
+
+/** Abstract base implementation of a {@link DigitalMultiMeter}.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public abstract class AbstractGpibDigitalMultiMeter
+  extends AbstractGpibInstrument
+  implements DigitalMultiMeter
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (AbstractGpibDigitalMultiMeter.class.getName ());
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  protected AbstractGpibDigitalMultiMeter (
+    final String name,
+    final GpibDevice device,
+    final List<Runnable> runnables,
+    final List<Service> targetServices,
+    final boolean addStatusServices,
+    final boolean addSettingsServices,
+    final boolean addCommandProcessorServices,
+    final boolean addAcquisitionServices)
+
+  {
+    super (name, device, runnables, targetServices,
+      addStatusServices, addSettingsServices, addCommandProcessorServices, addAcquisitionServices);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DigitalMultiMeter
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public void setResolution (final NumberOfDigits resolution) throws IOException, InterruptedException
+  {
+    addCommand (new DefaultInstrumentCommand (
+      InstrumentCommand.IC_RESOLUTION,
+      InstrumentCommand.ICARG_RESOLUTION, resolution));
+  }
+  
+  @Override
+  public void setMeasurementMode (final MeasurementMode measurementMode)
+    throws IOException, InterruptedException
+  {
+    addCommand (new DefaultInstrumentCommand (
+      InstrumentCommand.IC_MEASUREMENT_MODE,
+      InstrumentCommand.ICARG_MEASUREMENT_MODE, measurementMode));
+  }
+
+  @Override
+  public void setAutoRange () throws IOException, InterruptedException
+  {
+    addCommand (new DefaultInstrumentCommand (
+      InstrumentCommand.IC_AUTO_RANGE));
+  }
+  
+  @Override
+  public void setRange (final Range range) throws IOException, InterruptedException
+  {
+    addCommand (new DefaultInstrumentCommand (
+      InstrumentCommand.IC_RANGE,
+      InstrumentCommand.ICARG_RANGE, range));
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
