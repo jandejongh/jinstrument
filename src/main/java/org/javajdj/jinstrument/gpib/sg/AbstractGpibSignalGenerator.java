ef96764d29bd863eecb51b0a45480a552e54aa5d
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-02 23:16:27 +0100

    Moved class; refactoring; some javadoc.

---
+++ src/main/java/org/javajdj/jinstrument/gpib/sg/AbstractGpibSignalGenerator.java
@@ -0,0 +1,62 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.gpib.sg;
+
+import org.javajdj.jinstrument.SignalGenerator;
+import java.util.List;
+import org.javajdj.jinstrument.gpib.AbstractGpibInstrument;
+import org.javajdj.jinstrument.controller.gpib.GpibDevice;
+import org.javajdj.jservice.Service;
+
+/** Abstract base implementation of {@link SignalGenerator} for GPIB instruments.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ *
+ */
+public abstract class AbstractGpibSignalGenerator
+  extends AbstractGpibInstrument
+  implements SignalGenerator
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  protected AbstractGpibSignalGenerator (
+    final String name,
+    final GpibDevice device,
+    final List<Runnable> runnables,
+    final List<Service> targetServices,
+    final boolean addStatusServices,
+    final boolean addSettingsServices,
+    final boolean addCommandProcessorServices,
+    final boolean addAcquisitionServices)
+
+  {
+    super (name, device, runnables, targetServices,
+      addStatusServices, addSettingsServices, addCommandProcessorServices, addAcquisitionServices);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+}
