73a45be1bbfc9cfefa66171e51357cafa8b65901
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-03 12:17:54 +0100

    Added registration of HP70000_GPIB_Instrument.INSTRUMENT_TYPE.

--- src/main/java/org/javajdj/jinstrument/swing/JInstrument.java
+++ src/main/java/org/javajdj/jinstrument/swing/JInstrument.java
@@ -70,6 +70,7 @@ import org.javajdj.jinstrument.gpib.fc.hp5316a.HP5316A_GPIB_Instrument;
 import org.javajdj.jinstrument.gpib.fc.hp5328a.HP5328A_GPIB_Instrument;
 import org.javajdj.jinstrument.gpib.fg.hp8116a.HP8116A_GPIB_Instrument;
 import org.javajdj.jinstrument.gpib.none.NONE_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.sa.hp70000.HP70000_GPIB_Instrument;
 import org.javajdj.jinstrument.gpib.sa.hp8566b.HP8566B_GPIB_Instrument;
 import org.javajdj.jinstrument.gpib.sg.hp8663a.HP8663A_GPIB_Instrument;
 
@@ -120,6 +121,7 @@ extends JFrame
     this.instrumentRegistry.addInstrumentType (HP8566B_GPIB_Instrument.INSTRUMENT_TYPE);
     this.instrumentRegistry.addInstrumentType (HP8663A_GPIB_Instrument.INSTRUMENT_TYPE);
     this.instrumentRegistry.addInstrumentType (HP54502A_GPIB_Instrument.INSTRUMENT_TYPE);
+    this.instrumentRegistry.addInstrumentType (HP70000_GPIB_Instrument.INSTRUMENT_TYPE);
     this.instrumentRegistry.addInstrumentType (Tek2440_GPIB_Instrument.INSTRUMENT_TYPE);
     this.instrumentRegistry.addInstrumentViewType (JControllerDebug.INSTRUMENT_VIEW_TYPE);
     this.instrumentRegistry.addInstrumentViewType (JGpibDeviceConsole.INSTRUMENT_VIEW_TYPE);
