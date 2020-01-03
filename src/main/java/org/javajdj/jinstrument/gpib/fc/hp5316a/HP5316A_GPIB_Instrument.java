d49d7cc9a39e2353cc7b36b51ef617783f29d3ed
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-03 12:06:52 +0100

    Implementation of {@link Instrument} and {@link FrequencyCounter} for the HP-5316A.

---
+++ src/main/java/org/javajdj/jinstrument/gpib/fc/hp5316a/HP5316A_GPIB_Instrument.java
@@ -0,0 +1,256 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.gpib.fc.hp5316a;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.javajdj.jinstrument.controller.gpib.GpibDevice;
+import org.javajdj.jinstrument.Device;
+import org.javajdj.jinstrument.DeviceType;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentCommand;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentType;
+import org.javajdj.jinstrument.controller.gpib.DeviceType_GPIB;
+import org.javajdj.jinstrument.FrequencyCounter;
+import org.javajdj.jinstrument.gpib.fc.AbstractGpibFrequencyCounter;
+
+/** Implementation of {@link Instrument} and {@link FrequencyCounter} for the HP-5316A.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class HP5316A_GPIB_Instrument
+extends AbstractGpibFrequencyCounter
+implements FrequencyCounter
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (HP5316A_GPIB_Instrument.class.getName ());
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public HP5316A_GPIB_Instrument (final GpibDevice device)
+  {
+    super ("HP-5316A", device, null, null, false, false, false, false /* XXX */ /* true, true, true, false */);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentType INSTRUMENT_TYPE = new InstrumentType ()
+  {
+    
+    @Override
+    public final String getInstrumentTypeUrl ()
+    {
+      return "HP-5316A [GPIB]";
+    }
+    
+    @Override
+    public final DeviceType getDeviceType ()
+    {
+      return DeviceType_GPIB.getInstance ();
+    }
+
+    @Override
+    public final HP5316A_GPIB_Instrument openInstrument (final Device device)
+    {
+      if (device == null || ! (device instanceof GpibDevice))
+      {
+        LOG.log (Level.WARNING, "Incompatible Device (not a GpibDevice): {0}!", device);
+        return null;
+      }
+      return new HP5316A_GPIB_Instrument ((GpibDevice) device);
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // Instrument
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentUrl ()
+  {
+    return HP5316A_GPIB_Instrument.INSTRUMENT_TYPE.getInstrumentTypeUrl () + "@" + getDevice ().getDeviceUrl ();
+  }
+
+  @Override
+  public String toString ()
+  {
+    return getInstrumentUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // Instrument
+  // POWER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public boolean isPoweredOn ()
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  @Override
+  public void setPoweredOn (final boolean poweredOn)
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // INSTRUMENT STATUS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  @Override
+  protected InstrumentStatus getStatusFromInstrumentSync ()
+    throws IOException, InterruptedException, TimeoutException
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  @Override
+  protected void requestStatusFromInstrumentASync ()
+    throws UnsupportedOperationException, IOException
+  {
+    throw new UnsupportedOperationException ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // INSTRUMENT SETTINGS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  protected void requestSettingsFromInstrumentASync () throws UnsupportedOperationException, IOException
+  {
+    throw new UnsupportedOperationException ();
+  }
+  
+  @Override
+  public InstrumentSettings getSettingsFromInstrumentSync ()
+    throws IOException, InterruptedException, TimeoutException
+  {
+    throw new UnsupportedOperationException ();
+  }
+ 
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // INSTRUMENT READING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  @Override
+  protected final InstrumentReading getReadingFromInstrumentSync () throws IOException, InterruptedException, TimeoutException
+  {
+    throw new UnsupportedOperationException ();
+  }
+
+  @Override
+  protected final void requestReadingFromInstrumentASync () throws IOException
+  {
+    throw new UnsupportedOperationException ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // FrequencyCounter
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // AbstractInstrument
+  // PROCESS COMMAND
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+  @Override
+  protected void processCommand (final InstrumentCommand instrumentCommand)
+    throws IOException, InterruptedException, TimeoutException
+  {
+    if (instrumentCommand == null)
+      throw new IllegalArgumentException ();
+    if (! instrumentCommand.containsKey (InstrumentCommand.IC_COMMAND_KEY))
+      throw new IllegalArgumentException ();
+    if (instrumentCommand.get (InstrumentCommand.IC_COMMAND_KEY) == null)
+      throw new IllegalArgumentException ();
+    if (! (instrumentCommand.get (InstrumentCommand.IC_COMMAND_KEY) instanceof String))
+      throw new IllegalArgumentException ();
+    final String commandString = (String) instrumentCommand.get (InstrumentCommand.IC_COMMAND_KEY);
+    if (commandString.trim ().isEmpty ())
+      throw new IllegalArgumentException ();
+    final GpibDevice device = (GpibDevice) getDevice ();
+    InstrumentSettings newInstrumentSettings = null;
+    try
+    {
+      switch (commandString)
+      {
+        case InstrumentCommand.IC_NOP_KEY:
+          break;
+        case InstrumentCommand.IC_GET_SETTINGS_KEY:
+        {
+          newInstrumentSettings = getSettingsFromInstrumentSync ();
+          break;
+        }
+        default:
+          throw new UnsupportedOperationException ();
+      }
+    }
+    finally
+    {
+      // EMPTY
+    }
+    if (newInstrumentSettings != null)
+      settingsReadFromInstrument (newInstrumentSettings);
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+}
