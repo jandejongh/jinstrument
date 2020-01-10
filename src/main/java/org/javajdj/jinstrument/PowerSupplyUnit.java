500ecbe60c734d863775352e1d6217394dc6d4e2
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-10 21:09:50 +0100

    Copyright date.

--- src/main/java/org/javajdj/jinstrument/PowerSupplyUnit.java
+++ src/main/java/org/javajdj/jinstrument/PowerSupplyUnit.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2010-2020 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
