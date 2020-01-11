518ab267242b4f97ec617a0cf1dd17af7fc95314
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-11 15:57:14 +0100

    Default implementation of {@link PowerSupplyUnitReading}.

---
+++ src/main/java/org/javajdj/jinstrument/DefaultPowerSupplyUnitReading.java
@@ -0,0 +1,162 @@
+/* 
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument;
+
+import java.util.logging.Logger;
+
+/** Default implementation of {@link PowerSupplyUnitReading}.
+ * 
+ * <p>
+ * The object is immutable.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ *
+ */
+public class DefaultPowerSupplyUnitReading
+  extends AbstractInstrumentReading
+  implements PowerSupplyUnitReading
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (DefaultPowerSupplyUnitReading.class.getName ());
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / CLONING / FACTORIES
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public DefaultPowerSupplyUnitReading (
+    final PowerSupplyUnitSettings settings,
+    final double readingVoltage_V,
+    final double readingCurrent_A,
+    final double readingPower_W,
+    final boolean error,
+    final String errorMessage,
+    final boolean uncalibrated,
+    final boolean uncorrected)
+  {
+    super (
+      settings,
+      new DefaultReading (readingVoltage_V, readingCurrent_A, readingPower_W),
+      null,
+      error,
+      errorMessage,
+      uncalibrated,
+      uncorrected);
+  }
+
+  @Override
+  public DefaultPowerSupplyUnitReading clone () throws CloneNotSupportedException
+  {
+    return (DefaultPowerSupplyUnitReading) super.clone ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentReading
+  // PowerSupplyUnitReading
+  // INSTRUMENT SETTINGS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  @Override
+  public PowerSupplyUnitSettings getInstrumentSettings ()
+  {
+    return (PowerSupplyUnitSettings) super.getInstrumentSettings ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // PowerSupplyUnitReading
+  // READING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public static class DefaultReading
+    implements Reading
+  {
+
+    public DefaultReading (final double readingVoltage_V, final double readingCurrent_A, final double readingPower_W)
+    {
+      this.readingVoltage_V = readingVoltage_V;
+      this.readingCurrent_A = readingCurrent_A;
+      this.readingPower_W = readingPower_W;
+    }
+    
+    private final double readingVoltage_V;
+
+    @Override
+    public final double getReadingVoltage_V ()
+    {
+      return this.readingVoltage_V;
+    }
+
+    private final double readingCurrent_A;
+    
+    @Override
+    public final double getReadingCurrent_A ()
+    {
+      return this.readingCurrent_A;
+    }
+
+    private final double readingPower_W;
+    
+    @Override
+    public final double getReadingPower_W ()
+    {
+      return this.readingPower_W;
+    }
+    
+  }
+  
+  @Override
+  public Reading getReadingValue ()
+  {
+    return (Reading) super.getReadingValue ();
+  }
+
+  @Override
+  public final double getReadingVoltage_V ()
+  {
+    return getReadingValue ().getReadingVoltage_V ();
+  }
+
+  @Override
+  public final double getReadingCurrent_A ()
+  {
+    return getReadingValue ().getReadingCurrent_A ();
+  }
+
+  @Override
+  public final double getReadingPower_W ()
+  {
+    return getReadingValue ().getReadingPower_W ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
