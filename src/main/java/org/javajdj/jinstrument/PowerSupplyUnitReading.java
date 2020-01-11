00dc6d8a9e68a67f009065ab4399e06ffca4225d
Jan de Jongh <jfcmdejongh@gmail.com>
2020-01-11 15:56:47 +0100

    A reading from a {@link PowerSupplyUnit}.

---
+++ src/main/java/org/javajdj/jinstrument/PowerSupplyUnitReading.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument;
+
+/** A reading from a {@link PowerSupplyUnit}.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public interface PowerSupplyUnitReading
+  extends InstrumentReading
+{
+
+  interface Reading
+  {
+    double getReadingVoltage_V ();
+    double getReadingCurrent_A ();
+    double getReadingPower_W ();
+  }
+  
+  @Override
+  PowerSupplyUnitSettings getInstrumentSettings ();
+
+  @Override
+  Reading getReadingValue ();
+
+  double getReadingVoltage_V ();
+  double getReadingCurrent_A ();
+  double getReadingPower_W ();
+  
+}
