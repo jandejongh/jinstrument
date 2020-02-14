faef998a68f8e3bce69a2c1fd5bc971384ffb52e
Jan de Jongh <jfcmdejongh@gmail.com>
2020-02-14 21:29:07 +0100

    Refactored: NumberOfDigits into separate class Resolution.

--- src/main/java/org/javajdj/jinstrument/DigitalMultiMeter.java
+++ src/main/java/org/javajdj/jinstrument/DigitalMultiMeter.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2010-2020 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,40 +27,6 @@ import java.util.List;
 public interface DigitalMultiMeter
 extends Instrument
 {
-
-  enum NumberOfDigits
-  {
-    DIGITS_1,
-    DIGITS_1_5,
-    DIGITS_2,
-    DIGITS_2_5,
-    DIGITS_3,
-    DIGITS_3_5,
-    DIGITS_4,
-    DIGITS_4_5,
-    DIGITS_5,
-    DIGITS_5_5,
-    DIGITS_6,
-    DIGITS_6_5,
-    DIGITS_7,
-    DIGITS_7_5,
-    DIGITS_8,
-    DIGITS_8_5,
-    DIGITS_9,
-    DIGITS_9_5,
-    DIGITS_10,
-    DIGITS_10_5,
-    DIGITS_11,
-    DIGITS_11_5,
-    DIGITS_12,
-    DIGITS_12_5,
-    DIGITS_13,
-    DIGITS_13_5,
-    DIGITS_14,
-    DIGITS_14_5,
-    DIGITS_15,
-    DIGITS_15_5;
-  }
   
   enum MeasurementMode
   {
@@ -86,7 +52,7 @@ extends Instrument
     Unit getUnit ();
   }
   
-  List<NumberOfDigits> getSupportedResolutions ();
+  List<Resolution> getSupportedResolutions ();
   
   List<MeasurementMode> getSupportedMeasurementModes ();
   
@@ -94,7 +60,7 @@ extends Instrument
 
   boolean supportsMeasurementMode (MeasurementMode measurementMode);
   
-  void setResolution (NumberOfDigits resolution)
+  void setResolution (Resolution resolution)
     throws IOException, InterruptedException;
     
   void setMeasurementMode (MeasurementMode measurementMode)
