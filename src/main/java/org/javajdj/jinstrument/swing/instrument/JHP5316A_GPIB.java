427d7f994636fbd028ce064b290e7fc436d459e0
Jan de Jongh <jfcmdejongh@gmail.com>
2020-10-11 20:09:29 +0200

    A Swing panel for (complete) control and status of a {@link JHP5316A_GPIB} Frequency Counter.
    Significant progress, but committing without knowing design/changes
    details (waited too long to commit...).

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/JHP5316A_GPIB.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright 2010-2020 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument;
+
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentListener;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.gpib.fc.hp5316a.HP5316A_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.fc.hp5316a.HP5316A_GPIB_Settings;
+import org.javajdj.jinstrument.swing.JDefaultFrequencyCounterView;
+
+/** A Swing panel for (complete) control and status of a {@link JHP5316A_GPIB} Frequency Counter.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JHP5316A_GPIB
+  extends JDefaultFrequencyCounterView<HP5316A_GPIB_Settings.MeasurementFunction, HP5316A_GPIB_Settings.TriggerSlope>
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JHP5316A_GPIB.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JHP5316A_GPIB (final HP5316A_GPIB_Instrument frequencyCounter, final int level)
+  {
+    //
+    super (frequencyCounter, level);
+    //
+    this.jInstrumentSpecificPanel1.removeAll ();
+    setPanelBorder (
+      this.jInstrumentSpecificPanel1,
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      "HP-5316A Specific");
+    // this.jInstrumentSpecificPanel1.setLayout (new GridLayout (4, 1));
+    //
+    this.jInstrumentSpecificPanel2.removeAll ();
+    setPanelBorder (
+      this.jInstrumentSpecificPanel2,
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      "HP-5316A Specific");
+    // this.jInstrumentSpecificPanel2.setLayout (new GridLayout (2, 1));
+    //
+    getFrequencyCounter ().addInstrumentListener (this.instrumentListener);
+    //
+  }
+  
+  public JHP5316A_GPIB (final HP5316A_GPIB_Instrument frequencyCounter)
+  {
+    this (frequencyCounter, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "HP-5316A [GPIB] FC View";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof HP5316A_GPIB_Instrument))
+        return new JHP5316A_GPIB ((HP5316A_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JHP5316A_GPIB.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING - COMPONENTS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING - LISTENERS
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT LISTENER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final InstrumentListener instrumentListener = new InstrumentListener ()
+  {
+    
+    @Override
+    public void newInstrumentStatus (final Instrument instrument, final InstrumentStatus instrumentStatus)
+    {
+      // EMPTY
+    }
+    
+    @Override
+    public void newInstrumentSettings (final Instrument instrument, final InstrumentSettings instrumentSettings)
+    {
+      if (instrument != JHP5316A_GPIB.this.getFrequencyCounter () || instrumentSettings == null)
+        throw new IllegalArgumentException ();
+      if (! (instrumentSettings instanceof HP5316A_GPIB_Settings))
+        throw new IllegalArgumentException ();
+      final HP5316A_GPIB_Settings settings = (HP5316A_GPIB_Settings) instrumentSettings;
+      SwingUtilities.invokeLater (() ->
+      {
+        JHP5316A_GPIB.this.inhibitInstrumentControl = true;
+        //
+        try
+        {
+        }
+        finally
+        {
+          JHP5316A_GPIB.this.inhibitInstrumentControl = false;
+        }
+      });
+    }
+
+    @Override
+    public void newInstrumentReading (final Instrument instrument, final InstrumentReading instrumentReading)
+    {
+      // EMPTY
+    }
+    
+  };
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
