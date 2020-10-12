587051a45e73ba1a79ec0d8fdf16cb21a0e3649e
Jan de Jongh <jfcmdejongh@gmail.com>
2020-10-12 20:31:06 +0200

    Default implementation of {@link DigitalStorageOscilloscopeSettings}.

---
+++ src/main/java/org/javajdj/jinstrument/DefaultDigitalStorageOscilloscopeSettings.java
@@ -0,0 +1,135 @@
+/* 
+ * Copyright 2010-2020 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+/** Default implementation of {@link DigitalStorageOscilloscopeSettings}.
+ * 
+ * <p>
+ * The object is immutable.
+ * 
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ *
+ */
+public class DefaultDigitalStorageOscilloscopeSettings
+  implements DigitalStorageOscilloscopeSettings
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (DefaultDigitalStorageOscilloscopeSettings.class.getName ());
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / CLONING / FACTORIES
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public DefaultDigitalStorageOscilloscopeSettings (
+    final byte[] bytes,
+    final Unit unit)
+  {
+    this.bytes = bytes;
+    this.unit = unit;
+  }
+
+  @Override
+  public DigitalStorageOscilloscopeSettings clone () throws CloneNotSupportedException
+  {
+    return (DigitalStorageOscilloscopeSettings) super.clone ();
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentSettings
+  // READING UNIT
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  private final Unit unit;
+  
+  @Override
+  public Unit getReadingUnit ()
+  {
+    return this.unit;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // DigitalStorageOscilloscopeSettings
+  // BYTES
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final byte[] bytes;
+  
+  @Override
+  public final byte[] getBytes ()
+  {
+    return this.bytes;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // EQUALS / HASHCODE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  @Override
+  public int hashCode ()
+  {
+    int hash = 7;
+    hash = 53 * hash + Arrays.hashCode (this.bytes);
+    return hash;
+  }
+
+  @Override
+  public boolean equals (Object obj)
+  {
+    if (this == obj)
+    {
+      return true;
+    }
+    if (obj == null)
+    {
+      return false;
+    }
+    if (getClass () != obj.getClass ())
+    {
+      return false;
+    }
+    final DefaultDigitalStorageOscilloscopeSettings other = (DefaultDigitalStorageOscilloscopeSettings) obj;
+    if (! Arrays.equals (this.bytes, other.bytes))
+    {
+      return false;
+    }
+    return true;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+}
