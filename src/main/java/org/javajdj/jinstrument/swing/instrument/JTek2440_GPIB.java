c5cb19c6d8821d24f0c6575aa862a88eb6f1ac16
Jan de Jongh <jfcmdejongh@gmail.com>
2020-10-17 17:38:25 +0200

    A Swing panel for (complete) control and status of a {@link Tek2440_GPIB_Instrument} Digital Storage Oscilloscope.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/JTek2440_GPIB.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2010-2020 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument;
+
+import java.util.logging.Logger;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.gpib.dso.tek2440.Tek2440_GPIB_Instrument;
+import org.javajdj.jinstrument.swing.JDefaultDigitalStorageOscilloscopeView;
+
+/** A Swing panel for (complete) control and status of a {@link Tek2440_GPIB_Instrument} Digital Storage Oscilloscope.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JTek2440_GPIB
+  extends JDefaultDigitalStorageOscilloscopeView
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JTek2440_GPIB.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JTek2440_GPIB (final Tek2440_GPIB_Instrument digitalStorageOscilloscope, final int level)
+  {
+    super (digitalStorageOscilloscope, level);
+  }
+  
+  public JTek2440_GPIB (final Tek2440_GPIB_Instrument digitalStorageOscilloscope)
+  {
+    this (digitalStorageOscilloscope, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "Tektronix 2440 [GPIB] DSO View";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof Tek2440_GPIB_Instrument))
+        return new JTek2440_GPIB ((Tek2440_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JTek2440_GPIB.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
