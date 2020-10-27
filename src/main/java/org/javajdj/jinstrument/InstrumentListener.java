f6b988767da29c0267a99665996e307e5862d0a3
Jan de Jongh <jfcmdejongh@gmail.com>
2020-10-27 21:10:41 +0100

    Added newInstrumentDebug [default -> empty] notification.

--- src/main/java/org/javajdj/jinstrument/InstrumentListener.java
+++ src/main/java/org/javajdj/jinstrument/InstrumentListener.java
@@ -42,4 +42,55 @@ public interface InstrumentListener
   
   void newInstrumentReading (Instrument instrument, InstrumentReading instrumentReading);
   
+  /** Notification of a debug-related event at an instrument.
+   * 
+   * <p>
+   * An optional method on an {@link InstrumentListener} for the purpose of debugging.
+   * The {@code debugId} is meant to distinguish between different debugging methods
+   * in place, so listeners can skip notifications from non-related debug exercises.
+   * The special value of zero for {@link debugId} means the issuer has not set it.
+   * 
+   * <P>
+   * Other than that, the are not really any further restriction on the use of this method.
+   * 
+   * <p>
+   * Unused arguments should be set to {@code null} or zero (for {@link @debugId}).
+   * 
+   * <p>
+   * The default implementation is empty.
+   * 
+   * @param instrument          The {@link Instrument} at which the event occurs.
+   * @param debugId             An identification of the debug method to which the event applies.
+   * @param instrumentStatus    An optional instrument status object.
+   * @param instrumentSettings  An optional instrument settings object.
+   * @param instrumentReading   An optional instrument reading object.
+   * @param debugObject1        An optional debug object.
+   * @param debuObject2         An optional debug object.
+   * @param debuObject3         An optional debug object.
+   * 
+   */
+  default void newInstrumentDebug (
+    Instrument instrument,
+    int debugId,
+    InstrumentStatus instrumentStatus,
+    InstrumentSettings instrumentSettings,
+    InstrumentReading instrumentReading,
+    Object debugObject1,
+    Object debuObject2,
+    Object debuObject3)
+  {
+  }
+  
+  /** Recommended value for the {@code debugId} argument in {@link #newInstrumentDebug} for debugging the
+   *  {@code byte} array in {@link InstrumentSettings}.
+   * 
+   * <p>
+   * The {@code byte} array is passed in the {@code debugObject1} argument, and the
+   * {@code instrument} argument should be set.
+   * 
+   * @see InstrumentSettings#getBytes
+   * 
+   */
+  static final int INSTRUMENT_DEBUG_ID_SETTINGS_BYTES_1 = 1;
+  
 }
