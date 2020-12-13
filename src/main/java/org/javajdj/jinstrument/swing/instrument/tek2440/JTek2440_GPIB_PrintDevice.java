f9ecbe71c8b2e8e5debc69e141b251685a6a5e8c
Jan de Jongh <jfcmdejongh@gmail.com>
2020-12-13 14:12:57 +0100

    Serious code cleanup and update in JTek2440_GPIB_PrintDevice.

--- src/main/java/org/javajdj/jinstrument/swing/instrument/tek2440/JTek2440_GPIB_PrintDevice.java
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/tek2440/JTek2440_GPIB_PrintDevice.java
@@ -16,32 +16,22 @@
  */
 package org.javajdj.jinstrument.swing.instrument.tek2440;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.logging.Logger;
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
-import javax.swing.SwingUtilities;
 import org.javajdj.jinstrument.DigitalStorageOscilloscope;
 import org.javajdj.jinstrument.Instrument;
-import org.javajdj.jinstrument.InstrumentListener;
-import org.javajdj.jinstrument.InstrumentReading;
-import org.javajdj.jinstrument.InstrumentSettings;
-import org.javajdj.jinstrument.InstrumentStatus;
 import org.javajdj.jinstrument.InstrumentView;
 import org.javajdj.jinstrument.InstrumentViewType;
 import org.javajdj.jinstrument.gpib.dso.tek2440.Tek2440_GPIB_Instrument;
 import org.javajdj.jinstrument.gpib.dso.tek2440.Tek2440_GPIB_Settings;
 import org.javajdj.jinstrument.swing.base.JDigitalStorageOscilloscopePanel;
-import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
 import static org.javajdj.jinstrument.swing.base.JInstrumentPanel.DEFAULT_MANAGEMENT_COLOR;
 import org.javajdj.jswing.jcenter.JCenter;
-import org.javajdj.jswing.jcolorcheckbox.JColorCheckBox;
 
 /** A Swing panel for the [Print] Device settings of a {@link Tek2440_GPIB_Instrument} Digital Storage Oscilloscope.
  *
@@ -87,16 +77,6 @@ public class JTek2440_GPIB_PrintDevice
       BorderFactory.createTitledBorder (
         BorderFactory.createLineBorder (DEFAULT_MANAGEMENT_COLOR, 2),
         "Description"));
-    jDescription.setContentType ("text/html");
-    jDescription.setBackground (getBackground ());
-    jDescription.setEditable (false);
-    jDescription.setText ("<html><b>Print Settings</b>" +
-//                          "<ul>" +
-//                            "<li> the data format for transferring settings, status, and traces;</li>" +
-//                            "<li>Features manual data transfers to and from the instrument;</li>" +
-//                            "<li>Values marked in red are used internally in the software and therefore read-only.</li>" +
-//                          "</ul>" +
-                          "</html>");
     add (jDescription);
     
     final JPanel printDeviceTypePanel = new JPanel ();
@@ -104,37 +84,13 @@ public class JTek2440_GPIB_PrintDevice
       BorderFactory.createTitledBorder (
         BorderFactory.createLineBorder (DEFAULT_MANAGEMENT_COLOR, 2),
         "[Print] Device Type"));
-    printDeviceTypePanel.setLayout (new GridLayout (1, 1, 0, 0));
-    this.jPrintDeviceType = new JComboBox<> (Tek2440_GPIB_Settings.PrintDeviceType.values ());
-    this.jPrintDeviceType.setSelectedItem (null);
-    this.jPrintDeviceType.setEditable (false);
-    this.jPrintDeviceType.setBackground (Color.red);
-    this.jPrintDeviceType.addItemListener (new JInstrumentComboBoxItemListener_Enum<> (
-      "print device type",
-      Tek2440_GPIB_Settings.PrintDeviceType.class,
-      (Instrument.InstrumentSetter_1Enum<Tek2440_GPIB_Settings.PrintDeviceType>) tek2440::setPrintDeviceType,
-      this::isInhibitInstrumentControl,
-      JInstrumentPanel.getGuiPreferencesUpdatePendingColor ()));
-    printDeviceTypePanel.add (JCenter.XY (this.jPrintDeviceType));
     add (printDeviceTypePanel);
-
+    
     final JPanel printDevicePageSizePanel = new JPanel ();
     printDevicePageSizePanel.setBorder (
       BorderFactory.createTitledBorder (
         BorderFactory.createLineBorder (DEFAULT_MANAGEMENT_COLOR, 2),
         "[Print Device] Page Size"));
-    printDevicePageSizePanel.setLayout (new GridLayout (1, 1, 0, 0));
-    this.jPrintDevicePageSize = new JComboBox<> (Tek2440_GPIB_Settings.PrintPageSize.values ());
-    this.jPrintDevicePageSize.setSelectedItem (null);
-    this.jPrintDevicePageSize.setEditable (false);
-    this.jPrintDevicePageSize.setBackground (Color.red);
-    this.jPrintDevicePageSize.addItemListener (new JInstrumentComboBoxItemListener_Enum<> (
-      "print page size",
-      Tek2440_GPIB_Settings.PrintPageSize.class,
-      (Instrument.InstrumentSetter_1Enum<Tek2440_GPIB_Settings.PrintPageSize>) tek2440::setPrintPageSize,
-      this::isInhibitInstrumentControl,
-      JInstrumentPanel.getGuiPreferencesUpdatePendingColor ()));
-    printDevicePageSizePanel.add (JCenter.XY (this.jPrintDevicePageSize));
     add (printDevicePageSizePanel);
     
     final JPanel pageLayoutPanel = new JPanel ();
@@ -142,39 +98,6 @@ public class JTek2440_GPIB_PrintDevice
       BorderFactory.createTitledBorder (
         BorderFactory.createLineBorder (DEFAULT_MANAGEMENT_COLOR, 2),
         "Page Layout"));
-    pageLayoutPanel.setLayout (new GridLayout (2, 4));
-    pageLayoutPanel.add (new JLabel ("Graticule"));
-    this.jPrintGraticule = new JColorCheckBox.JBoolean (Color.green);
-    this.jPrintGraticule.addActionListener (new JInstrumentActionListener_1Boolean (
-      "print graticule",
-      this.jPrintGraticule::getDisplayedValue,
-      tek2440::setPrintGraticule,
-      this::isInhibitInstrumentControl));
-    pageLayoutPanel.add (this.jPrintGraticule);
-    pageLayoutPanel.add (new JLabel ("Settings"));
-    this.jPrintSettings = new JColorCheckBox.JBoolean (Color.green);
-    this.jPrintSettings.addActionListener (new JInstrumentActionListener_1Boolean (
-      "print settings",
-      this.jPrintSettings::getDisplayedValue,
-      tek2440::setPrintSettings,
-      this::isInhibitInstrumentControl));
-    pageLayoutPanel.add (this.jPrintSettings);
-    pageLayoutPanel.add (new JLabel ("Text"));
-    this.jPrintText = new JColorCheckBox.JBoolean (Color.green);
-    this.jPrintText.addActionListener (new JInstrumentActionListener_1Boolean (
-      "print text",
-      this.jPrintText::getDisplayedValue,
-      tek2440::setPrintText,
-      this::isInhibitInstrumentControl));
-    pageLayoutPanel.add (this.jPrintText);
-    pageLayoutPanel.add (new JLabel ("Waveforms"));
-    this.jPrintWaveforms = new JColorCheckBox.JBoolean (Color.green);
-    this.jPrintWaveforms.addActionListener (new JInstrumentActionListener_1Boolean (
-      "print waveforms",
-      this.jPrintWaveforms::getDisplayedValue,
-      tek2440::setPrintWaveforms,
-      this::isInhibitInstrumentControl));
-    pageLayoutPanel.add (this.jPrintWaveforms);
     add (pageLayoutPanel);
     
     final JPanel commandPanel = new JPanel ();
@@ -182,12 +105,94 @@ public class JTek2440_GPIB_PrintDevice
       BorderFactory.createTitledBorder (
         BorderFactory.createLineBorder (DEFAULT_MANAGEMENT_COLOR, 2),
         "Print Command"));
-    commandPanel.setLayout (new BorderLayout ());
-    this.jPrintCommand = new JButton ("Print");
-    commandPanel.add (JCenter.XY (this.jPrintCommand), BorderLayout.CENTER);
     add (commandPanel);
     
-    getDigitalStorageOscilloscope ().addInstrumentListener (this.instrumentListener);
+    //
+    // jDescription
+    //
+    
+    jDescription.setContentType ("text/html");
+    jDescription.setBackground (getBackground ());
+    jDescription.setEditable (false);
+    jDescription.setText ("<html><b>Print Settings</b>" +
+                          "<ul>" +
+                            "<li>Defines the device type (ThinkJet or HPGL);</li>" +
+                            "<li>Defines the paper size (US or A4);</li>" +
+                            "<li>Defines what to include in the printout;</li>" +
+                            "<li>Provides the button to start printing.</li>" +
+                          "</ul>" +
+                          "</html>");
+
+    //
+    // printDeviceTypePanel
+    //
+    
+    printDeviceTypePanel.setLayout (new GridLayout (1, 1, 0, 0));
+    
+    printDeviceTypePanel.add (JCenter.XY (new JEnum_JComboBox<> (
+      Tek2440_GPIB_Settings.PrintDeviceType.class,
+      "print device type",
+      (settings) -> ((Tek2440_GPIB_Settings) settings).getPrintDeviceType (),
+      tek2440::setPrintDeviceType,
+      true)));
+    
+    //
+    // printDevicePageSizePanel
+    //
+    
+    printDevicePageSizePanel.setLayout (new GridLayout (1, 1, 0, 0));
+    
+    printDevicePageSizePanel.add (JCenter.XY (new JEnum_JComboBox<> (
+      Tek2440_GPIB_Settings.PrintPageSize.class,
+      "print page size",
+      (settings) -> ((Tek2440_GPIB_Settings) settings).getPrintDevicePageSize (),
+      tek2440::setPrintPageSize,
+      true)));
+    
+    //
+    // pageLayoutPanel
+    //
+    
+    pageLayoutPanel.setLayout (new GridLayout (2, 4));
+    
+    pageLayoutPanel.add (new JLabel ("Graticule"));
+    pageLayoutPanel.add (new JBoolean_JBoolean (
+      "print graticule",
+      (settings) -> ((Tek2440_GPIB_Settings) settings).isPrintGraticule (),
+      tek2440::setPrintGraticule,
+      Color.green,
+      true));
+    
+    pageLayoutPanel.add (new JLabel ("Settings"));
+    pageLayoutPanel.add (new JBoolean_JBoolean (
+      "print settings",
+      (settings) -> ((Tek2440_GPIB_Settings) settings).isPrintSettings (),
+      tek2440::setPrintSettings,
+      Color.green,
+      true));
+    
+    pageLayoutPanel.add (new JLabel ("Text"));
+    pageLayoutPanel.add (new JBoolean_JBoolean (
+      "print text",
+      (settings) -> ((Tek2440_GPIB_Settings) settings).isPrintText (),
+      tek2440::setPrintText,
+      Color.green,
+      true));
+    
+    pageLayoutPanel.add (new JLabel ("Waveforms"));
+    pageLayoutPanel.add (new JBoolean_JBoolean (
+      "print waveforms",
+      (settings) -> ((Tek2440_GPIB_Settings) settings).isPrintWaveforms (),
+      tek2440::setPrintWaveforms,
+      Color.green,
+      true));
+    
+    //
+    // commandPanel
+    //
+    
+    commandPanel.setLayout (new GridLayout (1, 1));
+    commandPanel.add (JCenter.XY (new JVoid_JColorCheckBox ("print", () -> { /* XXX */ }, Color.blue)));
     
   }
 
@@ -247,86 +252,6 @@ public class JTek2440_GPIB_PrintDevice
   {
     return getInstrumentViewUrl ();
   }
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // SWING
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  private final JComboBox<Tek2440_GPIB_Settings.PrintDeviceType> jPrintDeviceType;
-  
-  private final JComboBox<Tek2440_GPIB_Settings.PrintPageSize> jPrintDevicePageSize;
-  
-  private final JColorCheckBox.JBoolean jPrintGraticule;
-  
-  private final JColorCheckBox.JBoolean jPrintSettings;
-    
-  private final JColorCheckBox.JBoolean jPrintText;
-  
-  private final JColorCheckBox.JBoolean jPrintWaveforms;
-  
-  private final JButton jPrintCommand;
-  
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  //
-  // INSTRUMENT LISTENER
-  //
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  
-  private final InstrumentListener instrumentListener = new InstrumentListener ()
-  {
-    
-    @Override
-    public void newInstrumentStatus (final Instrument instrument, final InstrumentStatus instrumentStatus)
-    {
-      // EMPTY
-    }
-    
-    @Override
-    public void newInstrumentSettings (final Instrument instrument, final InstrumentSettings instrumentSettings)
-    {
-      if (instrument != JTek2440_GPIB_PrintDevice.this.getDigitalStorageOscilloscope () || instrumentSettings == null)
-        throw new IllegalArgumentException ();
-      if (! (instrumentSettings instanceof Tek2440_GPIB_Settings))
-        throw new IllegalArgumentException ();
-      final Tek2440_GPIB_Settings settings = (Tek2440_GPIB_Settings) instrumentSettings;
-      SwingUtilities.invokeLater (() ->
-      {
-        JTek2440_GPIB_PrintDevice.this.setInhibitInstrumentControl ();
-        try
-        {
-          JTek2440_GPIB_PrintDevice.this.jPrintDeviceType.setSelectedItem (
-            settings.getPrintDeviceType ());
-          JTek2440_GPIB_PrintDevice.this.jPrintDeviceType.setBackground (
-            JTek2440_GPIB_PrintDevice.this.getBackground ());
-          JTek2440_GPIB_PrintDevice.this.jPrintDevicePageSize.setSelectedItem (
-            settings.getPrintDevicePageSize ());
-          JTek2440_GPIB_PrintDevice.this.jPrintDevicePageSize.setBackground (
-            JTek2440_GPIB_PrintDevice.this.getBackground ());
-          JTek2440_GPIB_PrintDevice.this.jPrintGraticule.setDisplayedValue (
-            settings.isPrintGraticule ());
-          JTek2440_GPIB_PrintDevice.this.jPrintSettings.setDisplayedValue (
-            settings.isPrintSettings ());
-          JTek2440_GPIB_PrintDevice.this.jPrintText.setDisplayedValue (
-            settings.isPrintText ());
-          JTek2440_GPIB_PrintDevice.this.jPrintWaveforms.setDisplayedValue (
-            settings.isPrintWaveforms ());
-        }
-        finally
-        {
-          JTek2440_GPIB_PrintDevice.this.resetInhibitInstrumentControl ();
-        }
-      });
-    }
-
-    @Override
-    public void newInstrumentReading (final Instrument instrument, final InstrumentReading instrumentReading)
-    {
-      // EMPTY
-    }
-    
-  };
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
