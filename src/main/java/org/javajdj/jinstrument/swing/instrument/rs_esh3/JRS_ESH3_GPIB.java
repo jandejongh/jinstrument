aff464455f4fb58c51b0ce92c39eca47c4852837
Jan de Jongh <jfcmdejongh@gmail.com>
2021-11-26 20:09:07 +0100

    Typo in instrument-view-type URL string.

--- src/main/java/org/javajdj/jinstrument/swing/instrument/rs_esh3/JRS_ESH3_GPIB.java
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/rs_esh3/JRS_ESH3_GPIB.java
@@ -80,7 +80,7 @@ public class JRS_ESH3_GPIB
     @Override
     public final String getInstrumentViewTypeUrl ()
     {
-      return "Rohde&Schwarz ESH-3 [GPIB] DSO View";
+      return "Rohde&Schwarz ESH-3 [GPIB] SLM View";
     }
 
     @Override
