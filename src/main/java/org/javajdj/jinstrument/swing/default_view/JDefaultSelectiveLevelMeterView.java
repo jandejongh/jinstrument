573382cc398f7427f40f5bc58a0592739c98af52
Jan de Jongh <jfcmdejongh@gmail.com>
2021-11-26 19:45:17 +0100

    A one-size-fits-all Swing panel for control and status of a generic {@link SelectiveLevelMeter}.

---
+++ src/main/java/org/javajdj/jinstrument/swing/default_view/JDefaultSelectiveLevelMeterView.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2010-2021 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.default_view;
+
+import java.awt.GridLayout;
+import java.util.logging.Logger;
+import javax.swing.JPanel;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.SelectiveLevelMeter;
+import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
+import org.javajdj.jinstrument.swing.base.JSelectiveLevelMeterPanel;
+
+/** A one-size-fits-all Swing panel for control and status of a generic {@link SelectiveLevelMeter}.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JDefaultSelectiveLevelMeterView
+  extends JSelectiveLevelMeterPanel
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JDefaultSelectiveLevelMeterView.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JDefaultSelectiveLevelMeterView (final SelectiveLevelMeter selectiveLevelMeter, final int level)
+  {
+    super (selectiveLevelMeter, level);
+    setOpaque (true);
+    // setBackground (Color.white);
+    setLayout (new GridLayout (2, 2));
+    //
+    final JDefaultInstrumentManagementView instrumentManagementPanel= new JDefaultInstrumentManagementView (
+      JDefaultSelectiveLevelMeterView.this.getInstrument (),
+      JDefaultSelectiveLevelMeterView.this.getLevel () + 1);
+    instrumentManagementPanel.setPanelBorder (JInstrumentPanel.getGuiPreferencesManagementColor (), "Management");
+    add (instrumentManagementPanel);
+    //
+    add (new JPanel ());
+    //
+    add (new JPanel ());
+    //
+    add (new JPanel ());
+  }
+  
+  public JDefaultSelectiveLevelMeterView (final SelectiveLevelMeter selectiveLevelMeter)
+  {
+    this (selectiveLevelMeter, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "Default Selective Level Meter View";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof SelectiveLevelMeter))
+        return new JDefaultSelectiveLevelMeterView ((SelectiveLevelMeter) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JDefaultSelectiveLevelMeterView.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
