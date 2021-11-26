5402b2195d75eda28ef7f0f79701f6841b9a269d
Jan de Jongh <jfcmdejongh@gmail.com>
2021-11-26 19:57:43 +0100

    A Swing panel for (complete) control and status of a {@link JRS_ESH3_GPIB} Selective Level Meter.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/rs_esh3/JRS_ESH3_GPIB.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2010-2021 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument.rs_esh3;
+
+import java.util.logging.Logger;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentListener;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.gpib.slm.rs_esh3.RS_ESH3_GPIB_Instrument;
+import org.javajdj.jinstrument.swing.default_view.JDefaultSelectiveLevelMeterView;
+
+/** A Swing panel for (complete) control and status of a {@link JRS_ESH3_GPIB} Selective Level Meter.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JRS_ESH3_GPIB
+  extends JDefaultSelectiveLevelMeterView
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JRS_ESH3_GPIB.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JRS_ESH3_GPIB (final RS_ESH3_GPIB_Instrument selectiveLevelMeter, final int level)
+  {
+    
+    super (selectiveLevelMeter, level);
+    if (! (selectiveLevelMeter instanceof RS_ESH3_GPIB_Instrument))
+      throw new IllegalArgumentException ();
+    final RS_ESH3_GPIB_Instrument rs_esh3 = (RS_ESH3_GPIB_Instrument) selectiveLevelMeter;
+    
+    getSelectiveLevelMeter ().addInstrumentListener (this.instrumentListener);
+    
+  }
+  
+  public JRS_ESH3_GPIB (final RS_ESH3_GPIB_Instrument selectiveLevelMeter)
+  {
+    this (selectiveLevelMeter, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "Rohde&Schwarz ESH-3 [GPIB] DSO View";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof RS_ESH3_GPIB_Instrument))
+        return new JRS_ESH3_GPIB ((RS_ESH3_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JRS_ESH3_GPIB.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT LISTENER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final InstrumentListener instrumentListener = new InstrumentListener ()
+  {
+    
+    @Override
+    public void newInstrumentStatus (final Instrument instrument, final InstrumentStatus instrumentStatus)
+    {
+      if (instrument != JRS_ESH3_GPIB.this.getSelectiveLevelMeter () || instrumentStatus == null)
+        throw new IllegalArgumentException ();
+      // XXX
+//      if (! (instrumentStatus instanceof Tek2440_GPIB_Status))
+//        throw new IllegalArgumentException ();
+//      SwingUtilities.invokeLater (() ->
+//      {
+//        JRS_ESH3_GPIB.this.jSerialPollStatus.setDisplayedValue (
+//          ((Tek2440_GPIB_Status) instrumentStatus).getSerialPollStatusByte ());
+//      });
+    }
+    
+    @Override
+    public void newInstrumentSettings (final Instrument instrument, final InstrumentSettings instrumentSettings)
+    {
+      if (instrument != JRS_ESH3_GPIB.this.getSelectiveLevelMeter ()|| instrumentSettings == null)
+        throw new IllegalArgumentException ();
+//      if (! (instrumentSettings instanceof Tek2440_GPIB_Settings))
+//        throw new IllegalArgumentException ();
+//      final Tek2440_GPIB_Settings settings = (Tek2440_GPIB_Settings) instrumentSettings;
+      
+    }
+
+    @Override
+    public void newInstrumentReading (final Instrument instrument, final InstrumentReading instrumentReading)
+    {
+      // EMPTY
+    }
+    
+  };
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
