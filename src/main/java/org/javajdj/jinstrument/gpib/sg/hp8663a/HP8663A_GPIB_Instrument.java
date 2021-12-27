9b000deb44b815c71e9acd474fbabcd949fa90ce
Jan de Jongh <jfcmdejongh@gmail.com>
2021-12-27 11:03:26 +0100

    Retrofitted for changes in AbstractGpibInstrument. (onGpibServiceRequestFromInstrument Byte argument.)

--- src/main/java/org/javajdj/jinstrument/gpib/sg/hp8663a/HP8663A_GPIB_Instrument.java
+++ src/main/java/org/javajdj/jinstrument/gpib/sg/hp8663a/HP8663A_GPIB_Instrument.java
@@ -410,7 +410,7 @@ implements SignalGenerator
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
   @Override
-  protected void onGpibServiceRequestFromInstrument ()
+  protected void onGpibServiceRequestFromInstrument (final Byte statusByte)
     throws IOException, InterruptedException, TimeoutException
   {
     throw new UnsupportedOperationException ();
