9a229f0c3d7473270484230c18c098518dd89778
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-07 11:46:24 +0100

    Implementation of {@link InstrumentChannel} for the Rohde{@code &}Schwarz ESH-3 RF input channel.

---
+++ src/main/java/org/javajdj/jinstrument/gpib/slm/rs_esh3/RS_ESH3_GPIB_Channel_RF_Input.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.gpib.slm.rs_esh3;
+
+import org.javajdj.jinstrument.*;
+
+/** Implementation of {@link InstrumentChannel} for the Rohde{@code &}Schwarz ESH-3 RF input channel.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public final class RS_ESH3_GPIB_Channel_RF_Input
+  implements InstrumentChannel
+{
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private RS_ESH3_GPIB_Channel_RF_Input ()
+  {
+    // EMPTY
+  }
+  
+  private final static RS_ESH3_GPIB_Channel_RF_Input INSTANCE = new RS_ESH3_GPIB_Channel_RF_Input ();
+  
+  public final static RS_ESH3_GPIB_Channel_RF_Input getInstance ()
+  {
+    return INSTANCE;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
