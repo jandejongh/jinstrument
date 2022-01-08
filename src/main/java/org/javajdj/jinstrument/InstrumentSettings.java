d503433f8416a22599a545bba37140db48f96654
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-08 20:29:21 +0100

    Added [complete] javadoc.

--- src/main/java/org/javajdj/jinstrument/InstrumentSettings.java
+++ src/main/java/org/javajdj/jinstrument/InstrumentSettings.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,10 +21,12 @@ import org.javajdj.junits.Unit;
 /** The settings of an {@link Instrument}.
  *
  * <p>
+ * Implementations must be <i>immutable</i> and must properly implement the {@link #clone},
+ * {@link Object#equals} and {@link Object#hashCode} methods.
+ * 
+ * <p>
  * Objects implementing this interface carry the complete (relevant) settings of
  * an instrument.
- * It is highly recommended that such objects are immutable and equipped with
- * suitable implementations of {@link Object#equals} and {@link Object#hashCode}.
  * 
  * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
  * 
@@ -33,10 +35,68 @@ public interface InstrumentSettings
   extends Cloneable
 {
   
+  /** Creates a clone of this {@code Object}.
+   * 
+   * @return A clone of this {@code Object}.
+   * 
+   * @throws CloneNotSupportedException If cloning is not supported on this object (type);
+   *           this is an implementation error as it violates the {@link InstrumentSettings} contract.
+   * 
+   */
   InstrumentSettings clone () throws CloneNotSupportedException;
   
+  /** Returns an array of bytes uniquely identifying the settings of the {@link Instrument},
+   *    or {@code null} if this is unsupported.
+   * 
+   * <p>
+   * Many instruments support obtaining their complete settings as an array of bytes.
+   * If and only if the bytes equal both in length and contents between two distinct {@link InstrumentSettings} objects
+   * of the <i>same class</i>, then they are considered equal.
+   * Implementations must, if this notion is supported on the instrument in question.
+   * use a byte array to uniquely capture all instrument settings.
+   * However, instruments that do <i>not</i> support this,
+   * <i>must</i> set the bytes property to {@code null}
+   * on all relevant {@link InstrumentSettings} objects.
+   * 
+   * <p>
+   * Adherence to these specifications is crucial, as implementations and classes using
+   * {@link InstrumentSettings} often rely heavily on proper implementation of
+   * {@link Object#equals} and {@link Object#hashCode}, and the availability of a non-{@code null} bytes array
+   * greatly reduces complexity of proper implementation of these methods.
+   * 
+   * @return An array of bytes uniquely identifying the settings of the {@link Instrument}, may be {@code null}.
+   * 
+   * @see Object#equals
+   * @see Object#hashCode
+   * 
+   */
   byte[] getBytes ();
   
+  /** Returns the default {@link Unit} of readings obtained from the instrument given these settings.
+   * 
+   * <p>
+   * Some implementations of {@link Instrument} need to know the current settings of the instrument
+   * in order to determine what unit/meaning a reading from this instrument has.
+   * If so, implementation must properly set the reading {@link Unit} property on the settings.
+   * However, for many other implementations this is not needed as the readings themselves already
+   * contain sufficient information on what has been measured and against which {@link Unit}.
+   * Such implementations should return {@code null} here.
+   * 
+   * <p>
+   * Note that this method merely serves as a hint to {@link Instrument} implementations,
+   * so do not use this method in order to obtain {@link Unit}s on {@link InstrumentReading}s.
+   * Users of {@link InstrumentReading},
+   * in particular implementations of {@link InstrumentView},
+   * should always use {@link InstrumentReading#getUnit}
+   * instead of this method.
+   * 
+   * @return The default unit of readings from the instrument with these settings, may be {@code null}.
+   * 
+   * @see Instrument
+   * @see InstrumentReading
+   * @see InstrumentView
+   * 
+   */ 
   Unit getReadingUnit ();
   
 }
