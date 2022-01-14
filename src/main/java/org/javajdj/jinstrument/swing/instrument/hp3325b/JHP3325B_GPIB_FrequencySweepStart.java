1b3d219274895e2be44b0ac4de7991817d7ec920
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-14 21:17:45 +0100

    A Swing panel for the Start Frequency setting of a {@link HP3325B_GPIB_Instrument} Function Generator.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/hp3325b/JHP3325B_GPIB_FrequencySweepStart.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument.hp3325b;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import org.javajdj.jinstrument.FunctionGenerator;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentListener;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.gpib.fg.hp3325b.HP3325B_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.fg.hp3325b.HP3325B_GPIB_Settings;
+import org.javajdj.jinstrument.swing.base.JFunctionGeneratorPanel;
+import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
+import org.javajdj.jswing.jsevensegment.JSevenSegmentNumber;
+
+/** A Swing panel for the Start Frequency setting of a {@link HP3325B_GPIB_Instrument} Function Generator.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JHP3325B_GPIB_FrequencySweepStart
+  extends JFunctionGeneratorPanel
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JHP3325B_GPIB_FrequencySweepStart.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JHP3325B_GPIB_FrequencySweepStart (
+    final FunctionGenerator functionGenerator,
+    final String title,
+    final int level,
+    final Color panelColor)
+  {
+
+    super (functionGenerator, title, level, panelColor);
+    if (! (functionGenerator instanceof HP3325B_GPIB_Instrument))
+      throw new IllegalArgumentException ();
+    final HP3325B_GPIB_Instrument hp3325b = (HP3325B_GPIB_Instrument) functionGenerator;
+
+    removeAll ();
+    setLayout (new GridLayout (1, 1));
+    
+    this.jFreqStart = new JSevenSegmentNumber (JInstrumentPanel.getGuiPreferencesFrequencyColor (), false, 11, 7);
+    this.jFreqStart.addMouseListener (this.jFreqStartMouseListener);
+    add (this.jFreqStart);
+    
+    getFunctionGenerator ().addInstrumentListener (this.instrumentListener);
+
+  }
+
+  public JHP3325B_GPIB_FrequencySweepStart (final FunctionGenerator functionGenerator, final int level)
+  {
+    this (functionGenerator, null, level, null);
+  }
+  
+  public JHP3325B_GPIB_FrequencySweepStart (final FunctionGenerator functionGenerator)
+  {
+    this (functionGenerator, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "HP-3325B [GPIB] Start Frequency Setting";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof HP3325B_GPIB_Instrument))
+        return new JHP3325B_GPIB_FrequencySweepStart ((HP3325B_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JHP3325B_GPIB_FrequencySweepStart.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  // FREQUENCY
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final JSevenSegmentNumber jFreqStart;
+  
+  private final MouseListener jFreqStartMouseListener = new MouseAdapter ()
+  {
+    @Override
+    public void mouseClicked (final MouseEvent me)
+    {
+      final Double newFrequency_Hz = getFrequencyFromDialog_Hz ("Enter Start Frequency [Hz]", null);
+      if (newFrequency_Hz != null)
+        try
+        {
+          ((HP3325B_GPIB_Instrument) JHP3325B_GPIB_FrequencySweepStart.this.getFunctionGenerator ()).setSweepStartFrequency_Hz (
+            newFrequency_Hz);
+        }
+        catch (InterruptedException ie)
+        {
+           LOG.log (Level.INFO, "Caught InterruptedException while setting start frequency on instrument: {0}.",
+             ie.getStackTrace ());
+        }
+        catch (IOException ioe)
+        {
+           LOG.log (Level.INFO, "Caught IOException while setting start frequency on instrument: {0}.",
+             ioe.getStackTrace ());
+        }
+    }
+  };
+    
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT LISTENER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final InstrumentListener instrumentListener = new InstrumentListener ()
+  {
+    
+    @Override
+    public void newInstrumentStatus (final Instrument instrument, final InstrumentStatus instrumentStatus)
+    {
+      // EMPTY
+    }
+    
+    @Override
+    public void newInstrumentSettings (final Instrument instrument, final InstrumentSettings instrumentSettings)
+    {
+      if (instrument != JHP3325B_GPIB_FrequencySweepStart.this.getFunctionGenerator () || instrumentSettings == null)
+        throw new IllegalArgumentException ();
+      if (! (instrumentSettings instanceof HP3325B_GPIB_Settings))
+        throw new IllegalArgumentException ();
+      final HP3325B_GPIB_Settings settings = (HP3325B_GPIB_Settings) instrumentSettings;
+      SwingUtilities.invokeLater (() ->
+      {
+        JHP3325B_GPIB_FrequencySweepStart.this.setInhibitInstrumentControl ();
+        //
+        try
+        {
+          final double reading_Hz = settings.getSweepStartFrequency_Hz ();
+          JHP3325B_GPIB_FrequencySweepStart.this.jFreqStart.setNumber (reading_Hz);
+        }
+        finally
+        {
+          JHP3325B_GPIB_FrequencySweepStart.this.resetInhibitInstrumentControl ();
+        }
+      });
+    }
+
+    @Override
+    public void newInstrumentReading (final Instrument instrument, final InstrumentReading instrumentReading)
+    {
+      // EMPTY
+    }
+    
+  };
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
