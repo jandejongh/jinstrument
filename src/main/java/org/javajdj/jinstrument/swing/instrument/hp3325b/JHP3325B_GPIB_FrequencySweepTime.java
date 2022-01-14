91d7a222636c5f8187f70c3e67dce6e6822deafb
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-14 21:06:21 +0100

    A Swing panel for the Frequency Sweep Time setting of a {@link HP3325B_GPIB_Instrument} Function Generator.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/hp3325b/JHP3325B_GPIB_FrequencySweepTime.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument.hp3325b;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.SwingUtilities;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentListener;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.FunctionGenerator;
+import org.javajdj.jinstrument.gpib.fg.hp3325b.HP3325B_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.fg.hp3325b.HP3325B_GPIB_Settings;
+import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
+import org.javajdj.jinstrument.swing.base.JFunctionGeneratorPanel;
+import org.javajdj.jswing.jsevensegment.JSevenSegmentNumber;
+
+/** A Swing panel for the Frequency Sweep Time setting of a {@link HP3325B_GPIB_Instrument} Function Generator.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JHP3325B_GPIB_FrequencySweepTime
+  extends JFunctionGeneratorPanel
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JHP3325B_GPIB_FrequencySweepTime.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JHP3325B_GPIB_FrequencySweepTime (
+    final FunctionGenerator functionGenerator,
+    final String title,
+    final int level,
+    final Color panelColor)
+  {
+
+    super (functionGenerator, title, level, panelColor);
+    if (! (functionGenerator instanceof HP3325B_GPIB_Instrument))
+      throw new IllegalArgumentException ();
+    final HP3325B_GPIB_Instrument hp3325b = (HP3325B_GPIB_Instrument) functionGenerator;
+
+    removeAll ();
+    setLayout (new GridLayout (1, 1));
+    
+    this.jTime = new JSevenSegmentNumber (JInstrumentPanel.getGuiPreferencesTimeColor (), false, 7, 3);
+    this.jTime.addMouseListener (this.jTimeMouseListener);
+    add (this.jTime);
+    
+    getFunctionGenerator ().addInstrumentListener (this.instrumentListener);
+
+  }
+
+  public JHP3325B_GPIB_FrequencySweepTime (final HP3325B_GPIB_Instrument functionGenerator, final int level)
+  {
+    this (functionGenerator, null, level, null);
+  }
+  
+  public JHP3325B_GPIB_FrequencySweepTime (final HP3325B_GPIB_Instrument functionGenerator)
+  {
+    this (functionGenerator, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "HP-3325B [GPIB] Frequency Sweep Time Setting";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof HP3325B_GPIB_Instrument))
+        return new JHP3325B_GPIB_FrequencySweepTime ((HP3325B_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JHP3325B_GPIB_FrequencySweepTime.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  // FREQUENCY
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final JSevenSegmentNumber jTime;
+  
+  private final MouseListener jTimeMouseListener = new MouseAdapter ()
+  {
+    @Override
+    public void mouseClicked (final MouseEvent me)
+    {
+      final Double newTime_s = getPeriodFromDialog_s ("Enter Sweep Time [s]", null);
+      if (newTime_s != null)
+        try
+        {
+          ((HP3325B_GPIB_Instrument) JHP3325B_GPIB_FrequencySweepTime.this.getFunctionGenerator ()).setSweepTime_s (
+            newTime_s);
+        }
+        catch (InterruptedException ie)
+        {
+           LOG.log (Level.INFO, "Caught InterruptedException while setting frequency sweep time on instrument: {0}.",
+             ie.getStackTrace ());
+        }
+        catch (IOException ioe)
+        {
+           LOG.log (Level.INFO, "Caught IOException while setting frequency sweep time on instrument: {0}.",
+             ioe.getStackTrace ());
+        }
+    }
+  };
+   
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT LISTENER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final InstrumentListener instrumentListener = new InstrumentListener ()
+  {
+    
+    @Override
+    public void newInstrumentStatus (final Instrument instrument, final InstrumentStatus instrumentStatus)
+    {
+      // EMPTY
+    }
+    
+    @Override
+    public void newInstrumentSettings (final Instrument instrument, final InstrumentSettings instrumentSettings)
+    {
+      if (instrument != JHP3325B_GPIB_FrequencySweepTime.this.getFunctionGenerator ()|| instrumentSettings == null)
+        throw new IllegalArgumentException ();
+      if (! (instrumentSettings instanceof HP3325B_GPIB_Settings))
+        throw new IllegalArgumentException ();
+      final HP3325B_GPIB_Settings settings = (HP3325B_GPIB_Settings) instrumentSettings;
+      SwingUtilities.invokeLater (() ->
+      {
+        JHP3325B_GPIB_FrequencySweepTime.this.inhibitInstrumentControl = true;
+        //
+        try
+        {
+          final double rem_s = settings.getSweepTime_s ();
+          final int reading_s = (int) Math.floor (rem_s);
+          final double rem_ms = rem_s - reading_s;
+          final int reading_ms = (int) Math.floor (rem_ms * 1.0e3);
+          JHP3325B_GPIB_FrequencySweepTime.this.jTime.setNumber (reading_s + 1.0e-3 * reading_ms);
+        }
+        finally
+        {
+          JHP3325B_GPIB_FrequencySweepTime.this.inhibitInstrumentControl = false;
+        }
+      });
+    }
+
+    @Override
+    public void newInstrumentReading (final Instrument instrument, final InstrumentReading instrumentReading)
+    {
+      // EMPTY
+    }
+    
+  };
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
