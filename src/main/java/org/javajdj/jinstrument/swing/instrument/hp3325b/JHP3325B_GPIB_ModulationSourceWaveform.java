5f6ae690596df784ef294765cfe980b9811ff10e
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-14 21:13:24 +0100

    A Swing panel for the Modulation Source Waveform setting of a {@link HP3325B_GPIB_Instrument} Function Generator.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/hp3325b/JHP3325B_GPIB_ModulationSourceWaveform.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument.hp3325b;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.logging.Logger;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.FunctionGenerator;
+import org.javajdj.jinstrument.gpib.fg.hp3325b.HP3325B_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.fg.hp3325b.HP3325B_GPIB_Settings;
+import org.javajdj.jinstrument.swing.base.JFunctionGeneratorPanel;
+import org.javajdj.jswing.jcenter.JCenter;
+
+/** A Swing panel for the Modulation Source Waveform setting of a {@link HP3325B_GPIB_Instrument} Function Generator.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JHP3325B_GPIB_ModulationSourceWaveform
+  extends JFunctionGeneratorPanel
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JHP3325B_GPIB_ModulationSourceWaveform.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JHP3325B_GPIB_ModulationSourceWaveform (
+    final FunctionGenerator functionGenerator,
+    final String title,
+    final int level,
+    final Color panelColor)
+  {
+
+    super (functionGenerator, title, level, panelColor);
+    if (! (functionGenerator instanceof HP3325B_GPIB_Instrument))
+      throw new IllegalArgumentException ();
+    final HP3325B_GPIB_Instrument hp3325b = (HP3325B_GPIB_Instrument) functionGenerator;
+
+    removeAll ();
+    setLayout (new GridLayout (1, 1));
+
+    add (JCenter.XY (new JEnum_JComboBox<> (
+      HP3325B_GPIB_Instrument.ModulationSourceWaveformFunction.class,
+      "Waveform",
+      (final InstrumentSettings settings) -> ((HP3325B_GPIB_Settings) settings).getModulationSourceWaveformFunction (),
+      hp3325b::setModulationSourceWaveformFunction,
+      true)));
+
+  }
+
+  public JHP3325B_GPIB_ModulationSourceWaveform (final HP3325B_GPIB_Instrument functionGenerator, final int level)
+  {
+    this (functionGenerator, null, level, null);
+  }
+  
+  public JHP3325B_GPIB_ModulationSourceWaveform (final HP3325B_GPIB_Instrument functionGenerator)
+  {
+    this (functionGenerator, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "HP-3325B [GPIB] Modulation Source Waveform Setting";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof HP3325B_GPIB_Instrument))
+        return new JHP3325B_GPIB_ModulationSourceWaveform ((HP3325B_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JHP3325B_GPIB_ModulationSourceWaveform.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
