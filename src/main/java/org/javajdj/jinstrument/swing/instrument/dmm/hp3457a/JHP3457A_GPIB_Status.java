6aa8b2ea8a415741c741e5e3c6f265be3f8cf838
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-23 20:53:33 +0100

    A Swing panel for the status of a {@link HP3457A_GPIB_Instrument} Digital Multi Meter.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/dmm/hp3457a/JHP3457A_GPIB_Status.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument.dmm.hp3457a;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.util.Arrays;
+import java.util.logging.Logger;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import org.javajdj.jinstrument.DigitalMultiMeter;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentListener;
+import org.javajdj.jinstrument.InstrumentReading;
+import org.javajdj.jinstrument.InstrumentSettings;
+import org.javajdj.jinstrument.InstrumentStatus;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.gpib.dmm.hp3457a.HP3457A_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.dmm.hp3457a.HP3457A_GPIB_Status;
+import org.javajdj.jinstrument.swing.base.JDigitalMultiMeterPanel;
+import org.javajdj.jswing.jbyte.JByte;
+
+/** A Swing panel for the status of a {@link HP3457A_GPIB_Instrument} Digital Multi Meter.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ * @see HP3457A_GPIB_Status
+ * 
+ */
+public class JHP3457A_GPIB_Status
+  extends JDigitalMultiMeterPanel
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JHP3457A_GPIB_Status.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JHP3457A_GPIB_Status (
+    final DigitalMultiMeter digitalMultiMeter,
+    final String title,
+    final int level,
+    final Color panelColor)
+  {
+
+    super (digitalMultiMeter, title, level, panelColor);
+    if (! (digitalMultiMeter instanceof HP3457A_GPIB_Instrument))
+      throw new IllegalArgumentException ();
+    final HP3457A_GPIB_Instrument hp3457a = (HP3457A_GPIB_Instrument) digitalMultiMeter;
+
+    removeAll ();
+    setLayout (new GridLayout (2, 1));
+    
+    this.jSerialPollStatus = new JByte (Color.red,
+      Arrays.asList (new String[]{" X ", "RQS", "ERR", "RDY", "PON", " FP", "H/L", "SPC"}));
+    add (this.jSerialPollStatus);
+    
+    this.jMessage = new JTextArea (3, 1);
+    this.jMessage.setEditable (false);
+    add (new JScrollPane (this.jMessage));
+
+    getDigitalMultiMeter ().addInstrumentListener (this.instrumentListener);
+    
+  }
+
+  public JHP3457A_GPIB_Status (final HP3457A_GPIB_Instrument digitalMultiMeter, final int level)
+  {
+    this (digitalMultiMeter, null, level, null);
+  }
+  
+  public JHP3457A_GPIB_Status (final HP3457A_GPIB_Instrument digitalMultiMeter)
+  {
+    this (digitalMultiMeter, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "HP-3457A [GPIB] Status";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof HP3457A_GPIB_Instrument))
+        return new JHP3457A_GPIB_Status ((HP3457A_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JHP3457A_GPIB_Status.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final JByte jSerialPollStatus;
+  
+  private final JTextArea jMessage;
+    
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT LISTENER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final InstrumentListener instrumentListener = new InstrumentListener ()
+  {
+    
+    @Override
+    public void newInstrumentStatus (final Instrument instrument, final InstrumentStatus instrumentStatus)
+    {
+      if (instrument != JHP3457A_GPIB_Status.this.getDigitalMultiMeter ()|| instrumentStatus == null)
+        throw new IllegalArgumentException ();
+      if (! (instrumentStatus instanceof HP3457A_GPIB_Status))
+        throw new IllegalArgumentException ();
+      final HP3457A_GPIB_Status hp3457a_status = (HP3457A_GPIB_Status) instrumentStatus;
+      SwingUtilities.invokeLater (() ->
+      {
+        JHP3457A_GPIB_Status.this.jSerialPollStatus.setDisplayedValue (hp3457a_status.getSerialPollStatusByte ());
+        if (hp3457a_status.isError ())
+        {
+          // XXX Maybe the auxiliaryErrorsString needs reformatting (intermediate CR/LF between multiple error messages.
+          final String errorsString = hp3457a_status.getErrorsString ();
+          if (errorsString != null)
+            JHP3457A_GPIB_Status.this.jMessage.append (errorsString + "\n");
+        }
+        if (hp3457a_status.isHardwareError ())
+        {
+          final String auxiliaryErrorsString = hp3457a_status.getAuxiliaryErrorsString ();
+          if (auxiliaryErrorsString != null)
+            JHP3457A_GPIB_Status.this.jMessage.append (auxiliaryErrorsString + "\n");          
+        }
+      });
+    }
+    
+    @Override
+    public void newInstrumentSettings (final Instrument instrument, final InstrumentSettings instrumentSettings)
+    {
+      // EMPTY
+    }
+
+    @Override
+    public void newInstrumentReading (final Instrument instrument, final InstrumentReading instrumentReading)
+    {
+      // EMPTY
+    }
+    
+  };
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
