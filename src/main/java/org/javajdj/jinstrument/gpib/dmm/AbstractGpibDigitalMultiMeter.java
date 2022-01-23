b5cfea80b1d4502566c4c355dd95ef56552ca315
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-23 18:51:37 +0100

    Cosmetics.

--- src/main/java/org/javajdj/jinstrument/gpib/dmm/AbstractGpibDigitalMultiMeter.java
+++ src/main/java/org/javajdj/jinstrument/gpib/dmm/AbstractGpibDigitalMultiMeter.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2010-2020 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,8 @@ public abstract class AbstractGpibDigitalMultiMeter
     final boolean addHousekeepingServices,
     final boolean addServiceRequestPollingServices)
   {
-    super (name,
+    super (
+      name,
       device,
       runnables,
       targetServices,
