9fd98e5a407221fe686b5cfa79bdef24dfd867ad
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-28 19:03:29 +0100

    A Swing panel for State Control Commands of a {@link HP3457A_GPIB_Instrument} Digital Multi Meter.

---
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/dmm/hp3457a/JHP3457A_GPIB_State.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package org.javajdj.jinstrument.swing.instrument.dmm.hp3457a;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import org.javajdj.jinstrument.DigitalMultiMeter;
+import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentView;
+import org.javajdj.jinstrument.InstrumentViewType;
+import org.javajdj.jinstrument.gpib.dmm.hp3457a.HP3457A_GPIB_Instrument;
+import org.javajdj.jinstrument.swing.base.JDigitalMultiMeterPanel;
+import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
+import org.javajdj.jinstrument.swing.component.JDialogButton;
+import org.javajdj.jswing.jcenter.JCenter;
+
+/** A Swing panel for State Control Commands of a {@link HP3457A_GPIB_Instrument} Digital Multi Meter.
+ *
+ * @author Jan de Jongh {@literal <jfcmdejongh@gmail.com>}
+ * 
+ */
+public class JHP3457A_GPIB_State
+  extends JDigitalMultiMeterPanel
+  implements InstrumentView
+{
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // LOGGER
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private static final Logger LOG = Logger.getLogger (JHP3457A_GPIB_State.class.getName ());
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // CONSTRUCTOR(S) / FACTORY / CLONING
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public JHP3457A_GPIB_State (
+    final DigitalMultiMeter digitalMultiMeter,
+    final String title,
+    final int level,
+    final Color panelColor)
+  {
+
+    super (digitalMultiMeter, title, level, panelColor);
+    if (! (digitalMultiMeter instanceof HP3457A_GPIB_Instrument))
+      throw new IllegalArgumentException ();
+    final HP3457A_GPIB_Instrument hp3457a = (HP3457A_GPIB_Instrument) digitalMultiMeter;
+
+    removeAll ();
+    setLayout (new GridLayout (2, 3));
+
+    add (JCenter.XY (new JVoid_JColorCheckBox (
+      "Reset",
+      hp3457a::reset,
+      Color.blue)));
+    
+    add (JCenter.XY (new JVoid_JColorCheckBox (
+      "Preset",
+      hp3457a::preset,
+      Color.blue)));
+        
+    add (new JDialogButton (
+      hp3457a,
+      null, 
+      new Dimension (80, 80),
+      "Memory",
+      new Dimension (800, 600),
+      new JHP3457A_GPIB_State_Memory (
+        hp3457a,
+        "Memory",
+        level + 1,
+        JInstrumentPanel.getGuiPreferencesManagementColor ())));
+    
+    add (JCenter.XY (new JLabel ("Reset")));
+    
+    add (JCenter.XY (new JLabel ("Preset")));
+    
+    add (JCenter.XY (new JLabel ("Memory")));
+    
+  }
+
+  public JHP3457A_GPIB_State (final HP3457A_GPIB_Instrument digitalMultiMeter, final int level)
+  {
+    this (digitalMultiMeter, null, level, null);
+  }
+  
+  public JHP3457A_GPIB_State (final HP3457A_GPIB_Instrument digitalMultiMeter)
+  {
+    this (digitalMultiMeter, 0);
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // INSTRUMENT VIEW TYPE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  public final static InstrumentViewType INSTRUMENT_VIEW_TYPE = new InstrumentViewType ()
+  {
+    
+    @Override
+    public final String getInstrumentViewTypeUrl ()
+    {
+      return "HP-3457A [GPIB] State Control Commands";
+    }
+
+    @Override
+    public final InstrumentView openInstrumentView (final Instrument instrument)
+    {
+      if (instrument != null && (instrument instanceof HP3457A_GPIB_Instrument))
+        return new JHP3457A_GPIB_State ((HP3457A_GPIB_Instrument) instrument);
+      else
+        return null;
+    }
+    
+  };
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // InstrumentView
+  // URL / NAME / toString
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  @Override
+  public String getInstrumentViewUrl ()
+  {
+    return JHP3457A_GPIB_State.INSTRUMENT_VIEW_TYPE.getInstrumentViewTypeUrl ()
+      + "<>"
+      + getInstrument ().getInstrumentUrl ();
+  }
+  
+  @Override
+  public String toString ()
+  {
+    return getInstrumentViewUrl ();
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // END OF FILE
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+}
