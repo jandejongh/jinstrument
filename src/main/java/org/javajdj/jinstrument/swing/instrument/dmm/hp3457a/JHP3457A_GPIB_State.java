8dfce102b03ac7203231a4b60ee9c4bd798e2d3f
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-28 19:06:41 +0100

    Changed dialog titles.

--- src/main/java/org/javajdj/jinstrument/swing/instrument/dmm/hp3457a/JHP3457A_GPIB_State.java
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/dmm/hp3457a/JHP3457A_GPIB_State.java
@@ -84,11 +84,11 @@ public class JHP3457A_GPIB_State
       hp3457a,
       null, 
       new Dimension (80, 80),
-      "Memory",
+      "State Memory",
       new Dimension (800, 600),
       new JHP3457A_GPIB_State_Memory (
         hp3457a,
-        "Memory",
+        "State Memory",
         level + 1,
         JInstrumentPanel.getGuiPreferencesManagementColor ())));
     
