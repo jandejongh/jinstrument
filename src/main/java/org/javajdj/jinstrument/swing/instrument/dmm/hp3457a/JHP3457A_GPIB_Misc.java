2bf1c23ea22c6a40e26ae46fd6d3696e141f73ae
Jan de Jongh <jfcmdejongh@gmail.com>
2022-01-30 12:53:30 +0100

    Reached more or less final layout.

--- src/main/java/org/javajdj/jinstrument/swing/instrument/dmm/hp3457a/JHP3457A_GPIB_Misc.java
+++ src/main/java/org/javajdj/jinstrument/swing/instrument/dmm/hp3457a/JHP3457A_GPIB_Misc.java
@@ -17,13 +17,22 @@
 package org.javajdj.jinstrument.swing.instrument.dmm.hp3457a;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.util.logging.Logger;
 import org.javajdj.jinstrument.DigitalMultiMeter;
 import org.javajdj.jinstrument.Instrument;
+import org.javajdj.jinstrument.InstrumentSettings;
 import org.javajdj.jinstrument.InstrumentView;
 import org.javajdj.jinstrument.InstrumentViewType;
 import org.javajdj.jinstrument.gpib.dmm.hp3457a.HP3457A_GPIB_Instrument;
+import org.javajdj.jinstrument.gpib.dmm.hp3457a.HP3457A_GPIB_Settings;
 import org.javajdj.jinstrument.swing.base.JDigitalMultiMeterPanel;
+import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
+import static org.javajdj.jinstrument.swing.base.JInstrumentPanel.getGuiPreferencesManagementColor;
+import static org.javajdj.jinstrument.swing.base.JInstrumentPanel.getGuiPreferencesTimeColor;
+import org.javajdj.jswing.jcenter.JCenter;
+import org.javajdj.jswing.jcolorcheckbox.JDialogCheckBox;
 
 /** A Swing panel for Miscellaneous Settings of a {@link HP3457A_GPIB_Instrument} Digital Multi Meter.
  *
@@ -62,6 +71,148 @@ public class JHP3457A_GPIB_Misc
     final HP3457A_GPIB_Instrument hp3457a = (HP3457A_GPIB_Instrument) digitalMultiMeter;
 
     removeAll ();
+    setLayout (new GridLayout (5, 2));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Math",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (), 
+        "Math",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_Math (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Memory",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (),
+        "Memory",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_Memory (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "#PLCs",
+      level + 1,
+      getGuiPreferencesTimeColor (),
+      JCenter.XY (new Jdouble_JTextField (
+        Double.NaN,
+        8,
+        "#PLCs",
+        (final InstrumentSettings settings) -> ((HP3457A_GPIB_Settings) settings).getNumberOfPowerLineCycles (),
+        hp3457a::setNumberOfPowerLineCycles,
+        true))));
+      
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Subprograms",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (),
+        "Subprograms",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_Subprograms (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
+      
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Display",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (),
+        "Display",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_Display (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Audio",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JHP3457A_GPIB_Audio (
+        digitalMultiMeter,
+        null,
+        level + 1,
+        null)));
+        
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Reading Format",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JEnum_JComboBox<> (
+        HP3457A_GPIB_Settings.ReadingFormat.class,
+        "Reading Format",
+        (final InstrumentSettings settings) -> ((HP3457A_GPIB_Settings) settings).getReadingFormat (),
+        hp3457a::setReadingFormat,
+        true)));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Service",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (),
+        "Service",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_Service (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Options Control",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (),
+        "Options Control",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_OptionsControl (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
+    
+    add (new JInstrumentPanel (
+      hp3457a,
+      "Rare/Dangerous",
+      level + 1,
+      getGuiPreferencesManagementColor (),
+      new JDialogCheckBox (
+        getBackground ().darker (),
+        "Rare/Dangerous",
+        new Dimension (800, 600),
+        new JHP3457A_GPIB_Misc_RareDangerous (
+          hp3457a,
+          "",
+          level + 1,
+          JInstrumentPanel.getGuiPreferencesManagementColor ()))));
     
   }
 
