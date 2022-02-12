b6f7e3074069afd891bca1f6a77fa13e4d987d12
Jan de Jongh <jfcmdejongh@gmail.com>
2022-02-12 16:28:36 +0100

    Retrofitting to ControllerType: - Added getControllerClass ().

--- src/main/java/org/javajdj/jinstrument/controller/gpib/prologix/ProLogixGpibEthernetControllerType.java
+++ src/main/java/org/javajdj/jinstrument/controller/gpib/prologix/ProLogixGpibEthernetControllerType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -65,6 +65,12 @@ implements ControllerType
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+  @Override
+  public final Class<ProLogixGpibEthernetController> getControllerClass ()
+  {
+    return ProLogixGpibEthernetController.class;
+  }
+  
   public final static String PROLOGIX_GPIB_ETHERNET_CONTROLLER_TYPE_NAME = "prologix-eth";
   
   @Override
