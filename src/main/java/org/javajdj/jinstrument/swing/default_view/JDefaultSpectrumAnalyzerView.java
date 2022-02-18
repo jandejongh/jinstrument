d19736fd3dbc7545c467737db5659a701eeacdff
Jan de Jongh <jfcmdejongh@gmail.com>
2022-02-18 22:03:41 +0100

    - Added sub-class access to the panels; - Added options to populate the panels at constructions time   (from sub-classes); - Some pretty minor GUI changes.

--- src/main/java/org/javajdj/jinstrument/swing/default_view/JDefaultSpectrumAnalyzerView.java
+++ src/main/java/org/javajdj/jinstrument/swing/default_view/JDefaultSpectrumAnalyzerView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2019 Jan de Jongh <jfcmdejongh@gmail.com>.
+ * Copyright 2010-2022 Jan de Jongh <jfcmdejongh@gmail.com>.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,6 +19,7 @@ package org.javajdj.jinstrument.swing.default_view;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.util.logging.Logger;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import org.javajdj.jinstrument.Instrument;
 import org.javajdj.jinstrument.InstrumentView;
@@ -26,6 +27,7 @@ import org.javajdj.jinstrument.InstrumentViewType;
 import org.javajdj.jinstrument.SpectrumAnalyzer;
 import org.javajdj.jinstrument.swing.base.JInstrumentPanel;
 import org.javajdj.jinstrument.swing.base.JSpectrumAnalyzerPanel;
+import org.javajdj.jswing.jcenter.JCenter;
 
 /** A one-size-fits-all Swing panel for control and status of a generic {@link SpectrumAnalyzer}.
  *
@@ -51,31 +53,72 @@ public class JDefaultSpectrumAnalyzerView
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-  public JDefaultSpectrumAnalyzerView (final SpectrumAnalyzer spectrumAnalyzer, final int level)
+  protected JDefaultSpectrumAnalyzerView (
+    final SpectrumAnalyzer spectrumAnalyzer,
+    final int level,
+    final boolean populateInstrumentManagementPanel,
+    final boolean populateTracePanel,
+    final boolean populateSettingsPanel)
   {
     super (spectrumAnalyzer, level);
+
     setOpaque (true);
-    // setBackground (Color.white);
+
     setLayout (new GridLayout (2, 2));
-    final JDefaultInstrumentManagementView instrumentManagementPanel= new JDefaultInstrumentManagementView (
-      JDefaultSpectrumAnalyzerView.this.getInstrument (),
-      JDefaultSpectrumAnalyzerView.this.getLevel () + 1);
-    instrumentManagementPanel.setPanelBorder (JInstrumentPanel.getGuiPreferencesManagementColor (), "Management");
-    add (instrumentManagementPanel);
-    this.display = new JDefaultSpectrumAnalyzerTraceDisplay (spectrumAnalyzer, level + 1);
+    
+    this.jInstrumentManagementPanel = new JPanel ();
+    add (this.jInstrumentManagementPanel);
+    if (populateInstrumentManagementPanel)
+    {
+      this.jInstrumentManagementPanel.setLayout (new GridLayout (1, 1));
+      this.jInstrumentManagementPanel.add (new JDefaultInstrumentManagementView (
+        spectrumAnalyzer,
+        level + 1));
+    }
+    setPanelBorder (this.jInstrumentManagementPanel,
+      getLevel () + 1,
+      JInstrumentPanel.getGuiPreferencesManagementColor (),
+      "Management");
+    
+    this.jTracePanel = new JPanel ();
+    add (this.jTracePanel);
+    if (populateTracePanel)
+    {
+      this.jTracePanel.setLayout (new GridLayout (1, 1));
+      this.jTracePanel.add (new JDefaultSpectrumAnalyzerTraceDisplay (spectrumAnalyzer, level + 1));
+    }    
     // jinstrument Issue#0027.
-    //  JInstrumentPanel.setPanelBorder (this.display,
-    //    level + 1,
-    //    Color.pink,
-    //    "Trace");
-    add (this.display);
-    final JDefaultSpectrumAnalyzerSettingsPanel spectrumAnalyzerSettingsPanel = new JDefaultSpectrumAnalyzerSettingsPanel (
+    // setPanelBorder (this.jTracePanel,
+    //   level + 1,
+    //   Color.pink,
+    //  "Trace");
+    
+    this.jSettingsPanel = new JPanel ();
+    add (this.jSettingsPanel);
+    if (populateSettingsPanel)
+    {
+      this.jSettingsPanel.setLayout (new GridLayout (1, 1));
+      this.jSettingsPanel.add (new JDefaultSpectrumAnalyzerSettingsPanel (
         JDefaultSpectrumAnalyzerView.this.getSpectrumAnalyzer (),
         "Settings",
         JDefaultSpectrumAnalyzerView.this.getLevel () + 1,
-        Color.black);
-    add (spectrumAnalyzerSettingsPanel);
-    add (new JPanel ());
+        Color.black));
+    }
+    
+    this.jInstrumentSpecificPanel = new JPanel ();
+    add (this.jInstrumentSpecificPanel);
+    this.jInstrumentSpecificPanel.setLayout (new GridLayout (1, 1));
+    this.jInstrumentSpecificPanel.add (JCenter.XY (new JLabel ("Not supported in this View")));
+    setPanelBorder (this.jInstrumentSpecificPanel,
+      getLevel () + 1,
+      JInstrumentPanel.getGuiPreferencesManagementColor (),
+      "Instrument Specific");
+    
+  }
+  
+  public JDefaultSpectrumAnalyzerView (final SpectrumAnalyzer spectrumAnalyzer, final int level)
+  {
+    this (spectrumAnalyzer, level, true, true, true);
   }
   
   public JDefaultSpectrumAnalyzerView (final SpectrumAnalyzer spectrumAnalyzer)
@@ -132,12 +175,60 @@ public class JDefaultSpectrumAnalyzerView
   
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
-  // DISPLAY
+  // SWING
+  // INSTRUMENT MANAGEMENT PANEL
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  private final JPanel jInstrumentManagementPanel;
+  
+  protected final JPanel getInstrumentManagementPanel ()
+  {
+    return this.jInstrumentManagementPanel;
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  // TRACE PANEL
   //
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   
-  private final JDefaultSpectrumAnalyzerTraceDisplay display;
+  private final JPanel jTracePanel;
   
+  protected final JPanel getTracePanel ()
+  {
+    return this.jTracePanel;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  // SETTINGS PANEL
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+  private final JPanel jSettingsPanel;
+  
+  protected final JPanel getSettingsPanel ()
+  {
+    return this.jSettingsPanel;
+  }
+  
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  //
+  // SWING
+  // INSTRUMENT SPECIFIC PANEL
+  //
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  private final JPanel jInstrumentSpecificPanel;
+  
+  protected final JPanel getInstrumentSpecificPanel ()
+  {
+    return this.jInstrumentSpecificPanel;
+  }
+
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   //
   // END OF FILE
