372b9abe506b792954ad9434f504396a8de6bc8e
Jan de Jongh <jfcmdejongh@gmail.com>
2022-02-19 22:25:07 +0100

    javadoc [package-info].

---
+++ src/main/java/org/javajdj/jinstrument/gpib/fg/hp3325b/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2022 Jan de Jongh <jfcmdejongh@gmail.com>, TNO.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+/** {@link org.javajdj.jinstrument.Instrument} implementation for the GPIB (IEEE-488) controlled
+ *  HP-3325B {@link org.javajdj.jinstrument.FunctionGenerator}.
+ * 
+ **/
+package org.javajdj.jinstrument.gpib.fg.hp3325b;
